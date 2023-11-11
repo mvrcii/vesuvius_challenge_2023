03d26a959246269f41cea07e1e786a33a0aec908
Marcel Roth <MarcelRoth100@aol.com>
2023-11-12 00:14:58 +0100

    add files

--- unetr_segformer.py
+++ unetr_segformer.py
@@ -22,12 +22,22 @@ class UNETR_Segformer(nn.Module):
         self.upscaler2 = nn.ConvTranspose2d(
             1, 1, kernel_size=(4, 4), stride=2, padding=1)
 
+        # self.batch_norm = nn.BatchNorm2d(16) <- 16 = number of feature maps/channels (applied channel-wise)
+        # self.batch_norm_upscale1 = nn.BatchNorm2d(..)
+        # self.batch_norm_upscale2 = nn.BatchNorm2d(..)
+
     def forward(self, image):
-        output = self.encoder(image).max(axis=2)[0]  # 512, 512, 16 -> 512, 512, 16
+        output = self.encoder(image).max(axis=2)[0]  # 512, 512, 16 -> 512, 512, 16 (16 channels/feature maps??)
+        # TODO: Add BatchNorm2d/3D
+        # TODO: output = self.batch_norm(output)
         output = self.dropout(output)
         output = self.encoder_2d(output).logits
         output = self.upscaler1(output)
+        # TODO: Add BatchNorm2d/3D
+        # TODO: output = self.batch_norm_upscale1(output)
         output = self.upscaler2(output)
+        # TODO: Add BatchNorm2d/3D
+        # TODO: output = self.batch_norm_upscale2(output)
 
         output = output.squeeze(1)
         output = torch.sigmoid(output)
