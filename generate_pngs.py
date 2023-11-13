0bc68af80bae262cb7d4c2f6934ea49388f24dfa
JakeGonder <gonderjake@gmail.com>
2023-11-13 10:49:15 +0100

    update download for frag 4 this time

--- generate_pngs.py
+++ generate_pngs.py
@@ -3,6 +3,6 @@ import numpy as np
 
 prediction = np.load("frag_4_20231113-094614result.npy")
 plt.imshow(prediction, cmap='gray')
-plt.savefig('logits_fragment3.png', bbox_inches='tight', dpi=500, pad_inches=0)
+plt.savefig('logits_fragment4.png', bbox_inches='tight', dpi=500, pad_inches=0)
 plt.imshow(prediction > 0.5, cmap='gray')
-plt.savefig('binarized_fragment3.png', bbox_inches='tight', dpi=1500, pad_inches=0)
+plt.savefig('binarized_fragment4.png', bbox_inches='tight', dpi=1500, pad_inches=0)
