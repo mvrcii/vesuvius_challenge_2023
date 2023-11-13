75ee54388ca7ab13117a4c87db379fc332203996
JakeGonder <gonderjake@gmail.com>
2023-11-13 09:09:01 +0100

    add generate_pngs

---
+++ generate_pngs.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+prediction = np.load("20231113-080330result.npy")
+plt.imshow(prediction, cmap='gray')
+plt.savefig('logits.png', bbox_inches='tight', dpi=500, pad_inches=0)
+plt.imshow(prediction > 0.5, cmap='gray')
+plt.savefig('binarized.png', bbox_inches='tight', dpi=1500, pad_inches=0)
