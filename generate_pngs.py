2f8ff1790d2d1db861ffb7b6c853fb5437e5e720
JakeGonder <gonderjake@gmail.com>
2023-11-13 10:49:05 +0100

    update download for frag 3 this time

--- generate_pngs.py
+++ generate_pngs.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-prediction = np.load("20231113-093646result.npy")
+prediction = np.load("frag_4_20231113-094614result.npy")
 plt.imshow(prediction, cmap='gray')
 plt.savefig('logits_fragment3.png', bbox_inches='tight', dpi=500, pad_inches=0)
 plt.imshow(prediction > 0.5, cmap='gray')
