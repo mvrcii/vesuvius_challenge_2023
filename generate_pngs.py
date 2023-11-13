0f1a5949fd9497390210113898551c27faec3b35
JakeGonder <gonderjake@gmail.com>
2023-11-13 10:27:46 +0100

    update generate pngs for frag 3

--- generate_pngs.py
+++ generate_pngs.py
@@ -3,6 +3,6 @@ import numpy as np
 
 prediction = np.load("20231113-092302result.npy")
 plt.imshow(prediction, cmap='gray')
-plt.savefig('logits_fragment1.png', bbox_inches='tight', dpi=500, pad_inches=0)
+plt.savefig('logits_fragment3.png', bbox_inches='tight', dpi=500, pad_inches=0)
 plt.imshow(prediction > 0.5, cmap='gray')
-plt.savefig('binarized_fragment1.png', bbox_inches='tight', dpi=1500, pad_inches=0)
+plt.savefig('binarized_fragment3.png', bbox_inches='tight', dpi=1500, pad_inches=0)
