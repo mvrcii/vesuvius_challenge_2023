29358e85bbeea7332fbe81e5e92c7e92e3ffd0f2
Marcel Roth <MarcelRoth100@aol.com>
2023-11-15 03:52:53 +0100

    small changes

--- conf.py
+++ conf.py
@@ -42,8 +42,8 @@ class CFG:
     tile_size = 512
     stride = tile_size // 4
 
-    train_batch_size = 6  # 32
-    val_batch_size = 6
+    train_batch_size = 5  # 32
+    val_batch_size = 8
     num_workers = 2
 
     dataset_fraction = 1
