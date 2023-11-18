3c11e07cf188f2a900ad412ee232e5846f84678b
Marcel Roth <MarcelRoth100@aol.com>
2023-11-18 17:03:34 +0100

    add constants.py to give scroll ids synonyms

---
+++ constants.py
@@ -0,0 +1,23 @@
+OPTIMUS_FRAG_ID = "20231024093300"  # micha_frag
+BUMBLEBEE_FRAG_ID = "20230702185752"  # marcel_frag
+
+# Scrolls
+MEGATRON_FRAG_ID = "20230522181603"
+STARSCREAM_FRAG_ID = "20230827161847"
+SOUNDWAVE_FRAG_ID = "20230904135535"
+IRONHIDE_FRAG_ID = "20230905134255"
+RATCHET_FRAG_ID = "20230909121925"
+JAZZ_FRAG_ID = "20231016151000"
+
+
+# Additional Transformers character names for IDs
+# ULTRA_MAGNUS_FRAG_ID = "your_id_here_1"  # Replace 'your_id_here_1' with the actual ID
+# SHOCKWAVE_FRAG_ID = "your_id_here_2"  # Replace 'your_id_here_2' with the actual ID
+# HOT_ROD_FRAG_ID = "your_id_here_3"  # Replace 'your_id_here_3' with the actual ID
+# JETFIRE_FRAG_ID = "your_id_here_4"  # Replace 'your_id_here_4' with the actual ID
+# WHEELJACK_FRAG_ID = "your_id_here_5"  # Replace 'your_id_here_5' with the actual ID
+# BLASTER_FRAG_ID = "your_id_here_6"  # Replace 'your_id_here_6' with the actual ID
+# GRIMLOCK_FRAG_ID = "your_id_here_7"  # Replace 'your_id_here_7' with the actual ID
+# ARCEE_FRAG_ID = "your_id_here_8"  # Replace 'your_id_here_8' with the actual ID
+# GALVATRON_FRAG_ID = "your_id_here_9"  # Replace 'your_id_here_9' with the actual ID
+# DEVASTATOR_FRAG_ID = "your_id_here_10"  # Replace 'your_id_here_10' with the actual ID
