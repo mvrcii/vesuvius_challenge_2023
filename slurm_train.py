1b2dc858842e8904df81aeb1994aaa92f20ca1d2
Marcel Roth <MarcelRoth100@aol.com>
2023-11-20 01:37:09 +0100

    add slurm sbatch train script

---
+++ slurm_train.py
@@ -0,0 +1,32 @@
+import argparse
+import subprocess
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Submit a training job to Slurm.")
+    parser.add_argument("--gpu_type", type=str, default="c", choices=['a', 'b', 'c', '8a'],
+                        help="Type of GPU to use (a, b, c, 8a). Default is 'c'.")
+    parser.add_argument("--config_path", type=str, required=True,
+                        help="Path to the configuration file.")
+
+    args = parser.parse_args()
+
+    # Map the GPU type to the corresponding Slurm GPU resource
+    gpu_mapping = {
+        "a": "rtx3090:1",
+        "b": "rtx3090:1",
+        "c": "rtx4090:1",
+        "8a": "rtx2080ti:8"
+    }
+
+    gpu_resource = gpu_mapping[args.gpu_type]
+
+    # Construct the sbatch command
+    sbatch_cmd = f"sbatch -p ls6 --gres=gpu:{gpu_resource} --wrap=\"python pl_train.py {args.config_path}\" -o \"slurm_logs/slurm-%j.out\""
+
+    # Execute the sbatch command
+    subprocess.run(sbatch_cmd, shell=True)
+
+
+if __name__ == "__main__":
+    main()
