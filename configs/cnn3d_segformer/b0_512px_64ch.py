1e0ee68c22a124efe6eb3fa4095a4babfcddf903
Marcel Roth <MarcelRoth100@aol.com>
2023-11-20 08:57:00 +0100

    add cnn3d b0 config

---
+++ configs/cnn3d_segformer/b0_512px_64ch.py
@@ -0,0 +1,24 @@
+import os
+
+_base_ = [
+    "configs/datasets/single/optimus_bumblebee_512px_64ch.py",
+    "configs/schedules/adamw_cosine_lr.py",
+]
+
+work_dir = os.path.join("/scratch", "medfm", "vesuv", "kaggle1stReimp")
+base_label_dir = os.path.join("data", "base_label_files")
+data_root_dir = "data"
+dataset_target_dir = os.path.join("data", "datasets")
+
+model_type = "b0"
+architecture = 'cnn3d_segformer'
+model_name = f"{architecture}-{model_type}"
+from_pretrained = f"nvidia/mit-{model_type}"  # TODO: Refactor hardcoded from pretrained!
+in_chans = 64
+seed = 177
+epochs = -1
+
+dataset_fraction = 0.2
+num_workers = 4
+train_batch_size = 8
+val_batch_size = 8
