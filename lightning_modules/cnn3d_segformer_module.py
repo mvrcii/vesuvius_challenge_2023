66ca66a5edbd74679c6b00ea1112357d3dcc23c8
Marcel Roth <MarcelRoth100@aol.com>
2023-11-20 09:22:10 +0100

    fix

--- lightning_modules/cnn3d_segformer_module.py
+++ lightning_modules/cnn3d_segformer_module.py
@@ -5,7 +5,7 @@ from models.cnn3d_segformer import CNN3D_Segformer
 class CNN3D_SegformerModule(AbstractVesuvLightningModule):
     def __init__(self, cfg):
         super().__init__(cfg=cfg)
-        self.model = CNN3D_Segformer()
+        self.model = CNN3D_Segformer(cfg=cfg)
 
     def forward(self, x):
         output = self.model(x.unsqueeze(1).float())
