5ccdd917067096e6b90ed5ebf5109071dad431d3
Marcel Roth <MarcelRoth100@aol.com>
2023-11-23 12:15:27 +0100

    adds binarize_label_layered.py script to handle layered labels

---
+++ util/binarize_label_layered.py
@@ -0,0 +1,79 @@
+import os
+import sys
+
+import cv2
+import numpy as np
+from termcolor import colored
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+def process_image(img_src_dir, img_src_name, frag_id):
+    try:
+        target_dir = f"data/fragments/fragment{frag_id}/layered"
+        os.makedirs(target_dir, exist_ok=True)
+
+        pos = img_src_name.find('_')
+        file_name = img_src_name[pos+1:]
+
+        img_target_dir = os.path.join(target_dir, file_name)
+
+        # Load label image
+        label_img = cv2.imread(os.path.join(img_src_dir, img_src_name), 0)
+        label_arr = np.asarray(label_img)
+
+        # Binarize the label
+        binary_label = np.where(label_arr > 1, 1, 0)
+
+        # Scale the binary_label to full 8-bit range
+        scaled_label = (binary_label * 255).astype(np.uint8)
+
+        # Save the scaled image
+        cv2.imwrite(img_target_dir, scaled_label)
+
+        return True, img_target_dir
+    except Exception as e:
+        return False, str(e)
+
+
+def main():
+    success_count = 0
+    failure_count = 0
+
+    dir = "data/base_label_files/layered"
+
+    if len(os.listdir(dir)) == 0:
+        print("Error: No sub-directories found for:", dir)
+        return
+
+    for fragment_id in os.listdir(dir):
+
+        # Only process fragment directories within the layered folder
+        dir = os.path.join(dir, fragment_id)
+        if not os.path.isdir(dir):
+            print("Skipping element ", fragment_id)
+            continue
+
+        valid_file_count = 0
+        for file in os.listdir(dir):
+            if not file.endswith(".png") or not "inklabels" in file:
+                continue
+
+            valid_file_count += 1
+            success, message = process_image(img_src_dir=dir, img_src_name=file, frag_id=fragment_id)
+
+            if success:
+                print(colored(f"Success: Processed {message}", "green"))
+                success_count += 1
+            else:
+                print(colored(f"Failure: {message}", "red"))
+                failure_count += 1
+
+    print("\nReport:")
+    print(colored(f"Total Files: {valid_file_count}", "blue"))
+    print(colored(f"Total Successful: {success_count}", "green"))
+    print(colored(f"Total Failures: {failure_count}", "red"))
+
+
+if __name__ == "__main__":
+    main()
