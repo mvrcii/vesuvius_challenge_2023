8b5ee9298532535a32395e1f1d52746042bd5d00
JakeGonder <gonderjake@gmail.com>
2023-11-23 20:26:25 +0100

    add batch_infer_layered

---
+++ batch_infer_layered.py
@@ -0,0 +1,198 @@
+import os
+from datetime import datetime
+
+import cv2
+import numpy as np
+import torch
+from tqdm import tqdm
+from transformers import SegformerForSemanticSegmentation
+
+import conf_local as CFG
+
+'''
+        SET WORKING DIRECTORY TO PROJECT ROOT
+
+        Runs inference on full image, by slicing it into patches.
+'''
+
+
+def read_fragment(fragment_id, layer_start, layer_count):
+    images = []
+    print(f"attempting to read fragment {fragment_id} from layer {layer_start} to {layer_count - 1}")
+
+    for i in tqdm(range(layer_start, layer_start + layer_count)):
+        img_path = os.path.join(CFG.data_root_dir, "fragments", f"fragment{fragment_id}", "slices", f"{i:05}.tif")
+        print(img_path)
+
+        image = cv2.imread(img_path, 0)
+        print(image.shape)
+        assert 1 < np.asarray(image).max() <= 255, "Invalid image"
+
+
+        images.append(image)
+    images = np.stack(images, axis=0)
+
+    return images
+
+
+# Inference depends on:
+# - model
+# - in_channels
+# - patch_size_in
+# - patch_size_out
+
+def infer_full_fragment_layer(fragment_index, checkpoint_path, cfg, layer_start, layer_count=4):
+    # todo read checkpoint / channels from config
+    model = SegformerForSemanticSegmentation.from_pretrained("nvidia/mit-b0", num_labels=1,
+                                                             num_channels=4,
+                                                             ignore_mismatched_sizes=True)
+    model = model.to("cuda")
+    # checkpoint = torch.load(checkpoint_path)
+    # state_dict = {key.replace('model.', ''): value for key, value in checkpoint['state_dict'].items()}
+    # model.load_state_dict(state_dict)
+    # model.load_state_dict(checkpoint)
+    # print("Loaded model", checkpoint_path)
+
+    # Loading images
+    images = read_fragment(fragment_index, layer_start, layer_count)
+
+    # Define the size of the patches
+    patch_size = cfg.patch_size
+    expected_patch_shape = (CFG.in_channels, patch_size, patch_size)
+
+    # Todo get these from config?
+
+    patch_size_out = cfg.label_size
+    margin_percent = 0.1
+    stride_factor = 2
+
+    margin = int(margin_percent * patch_size_out)
+    mask = np.ones((patch_size_out, patch_size_out), dtype=bool)
+    mask[margin:-margin, margin:-margin] = False
+
+    stride = patch_size // stride_factor  # => 256
+    stride_out = patch_size_out // stride_factor  # => 64
+
+    height, width = images[0].shape
+
+    # Calculate the number of patches needed, considering the stride
+    x_patches = int(np.ceil((width - patch_size) / stride)) + 1
+    y_patches = int(np.ceil((height - patch_size) / stride)) + 1
+
+    # Initialize arrays to hold the stitched result and count of predictions for each pixel
+    out_height = y_patches * stride_out + patch_size_out
+    out_width = x_patches * stride_out + patch_size_out
+    out_arr = np.zeros((out_height, out_width), dtype=np.float32)
+    pred_counts = np.zeros((out_height, out_width), dtype=np.int32)
+
+    progress_bar = tqdm(total=x_patches * y_patches, desc="Infer Full Fragment: Processing patches")
+
+    # todo read batch_size_infer from config
+    batch_size = 4
+    batches = []
+    batch_indices = []
+
+    def process_patch(logits_np, x, y):
+        # Calculate the margin to ignore (10% of the patch size)
+        margin = int(0.1 * patch_size_out)
+
+        # Set the outer 10% of averaged_logits to zero
+        logits_np[mask] = 0
+
+        # Determine the location in the stitched_result array
+        out_y_start = y * stride_out
+        out_y_end = out_y_start + patch_size_out
+        out_x_start = x * stride_out
+        out_x_end = out_x_start + patch_size_out
+
+        # Add the result to the stitched_result array and increment prediction counts
+        out_arr[out_y_start:out_y_end, out_x_start:out_x_end] += logits_np
+        pred_counts[out_y_start + margin:out_y_end - margin, out_x_start + margin:out_x_end - margin] += 1
+
+    for y in range(y_patches):
+        for x in range(x_patches):
+            progress_bar.update(1)
+
+            x_start = x * stride
+            x_end = min(x_start + patch_size, width)
+            y_start = y * stride
+            y_end = min(y_start + patch_size, height)
+
+            patch = images[:, y_start:y_end, x_start:x_end]
+
+            # If the patch size is smaller than the expected size, skip it
+            if patch.shape != expected_patch_shape:
+                continue
+
+            batches.append(patch)
+            batch_indices.append((x, y))
+
+            # When batch is full, process it
+            if len(batches) == batch_size:
+                batch_tensor = torch.tensor(np.stack(batches)).float()
+                batch_tensor = batch_tensor.to("cuda")
+                outputs = model(batch_tensor)
+                logits = outputs.logits
+                logits_np = torch.sigmoid(logits).detach().squeeze().cpu().numpy()
+
+                for idx, (x, y) in enumerate(batch_indices):
+                    process_patch(logits_np[idx], x, y)  # Function to process each patch
+
+                batches = []
+                batch_indices = []
+
+    # Process any remaining patches
+    if batches:
+        batch_tensor = torch.tensor(np.stack(batches)).float()
+        outputs = model(batch_tensor)
+        logits = outputs.logits
+        logits_np = torch.sigmoid(logits).detach().squeeze().cpu().numpy()
+
+        for idx, (x, y) in enumerate(batch_indices):
+            process_patch(logits_np[idx], x, y)
+
+    progress_bar.close()
+    # Average the predictions
+    out_arr = np.where(pred_counts > 0, out_arr / pred_counts, 0)
+
+    return out_arr
+
+
+if __name__ == '__main__':
+    # if len(sys.argv) != 2:
+    #     print("Usage: python infer_full_fragment.py <checkpoint_path> <fragment_num> <config>")
+
+    # cfg = load_config(CFG)
+    # checkpoint_path = sys.argv[1]
+    # fragment_num = sys.argv[2]
+    fragment_num = 2
+    cfg = CFG
+    checkpoint_path = ""
+
+    date_time_string = datetime.now().strftime("%Y%m%d-%H%M%S")
+    results_dir = os.path.join("inference", "results", f"fragment{fragment_num}", date_time_string)
+    os.makedirs(results_dir, exist_ok=True)
+    print(f"Created directory {results_dir}")
+    # inference
+    for i in range(0, 61, cfg.in_channels):
+        print(f"Inferring layer {0} to {cfg.in_channels - 1}")
+        sigmoid_logits = infer_full_fragment_layer(fragment_num, checkpoint_path, cfg, layer_start=i,
+                                                   layer_count=cfg.in_channels)
+
+        np.save(os.path.join(results_dir, f"sigmoid_logits_{i}_{cfg.in_channels - 1}.npy"), sigmoid_logits)
+
+        # # save logits
+        # plt.imshow(result, cmap='gray')
+        # logit_path = os.path.join(results_dir, f"logits_{i}_{cfg.in_channels-1}.png")
+        # plt.savefig(logit_path, bbox_inches='tight', dpi=500, pad_inches=0)
+        #
+        # result_path = os.path.join(results_dir, f"logits_fragment{fragment_num}_{date_time_string}_scaled_norm.png")
+        # save_logits_scaled_normalized(result, result_path, image_shape)
+        #
+        # # save binary
+        # plt.imshow(result > 0.5, cmap='gray')
+        # binary_bath = os.path.join(results_dir, f"binarized_fragment{fragment_num}_{date_time_string}.png")
+        # plt.savefig(binary_bath, bbox_inches='tight', dpi=1500, pad_inches=0)
+        #
+        # print("Saved results to", results_dir)
+        # # save raw output
