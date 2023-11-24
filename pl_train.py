59cd31bd3a289704ee8d76f0826185de1069135a
Marcel Roth <MarcelRoth100@aol.com>
2023-11-24 22:38:18 +0100

    remove memory leak testing

--- pl_train.py
+++ pl_train.py
@@ -93,7 +93,7 @@ def main():
     trainer = Trainer(
         max_epochs=config.epochs,
         logger=wandb_logger,
-        callbacks=[LearningRateMonitor(logging_interval='epoch'), checkpoint_callback],
+        callbacks=[LearningRateMonitor(logging_interval='step'), checkpoint_callback],
         accelerator="auto",
         devices=devices,
         enable_progress_bar=True,
