5c3c959659aee1c5286aa8fb5d4f756d8405eda8
JakeGonder <gonderjake@gmail.com>
2023-11-26 15:49:35 +0100

    fix combine_predictions.py

--- inference/combine_predictions.py
+++ inference/combine_predictions.py
@@ -1,14 +1,32 @@
 import os
-from tkinter import Tk, Scale, HORIZONTAL, Label, Button
+from tkinter import Tk, Scale, HORIZONTAL, Label, Button, Frame, IntVar, Radiobutton
 
 import numpy as np
 from PIL import Image, ImageTk
 from tqdm import tqdm
 
+arrays = []
+threshold = 0.5
 
-def process_image(array, threshold, max_size=(1300, 800)):
+
+def process_image(array, threshold, max_size=(1800, 1000)):
+    processed = array.copy()
+
     # Apply threshold
-    processed = np.where(array > threshold, 1, 0)
+    # processed = np.where(array > threshold, 1, 0)
+
+    # reverse threshold
+    processed[processed < threshold] = 0
+    # processed = array
+
+    # Brightness
+    # image = array
+    # current_min = np.min(image)
+    # current_max = np.max(image)
+    # new_max = 1
+    # new_min = threshold
+    # processed = (image - current_min) / (current_max - current_min) * (new_max - new_min) + new_min
+
     image = Image.fromarray(np.uint8(processed * 255), 'L')
 
     # Resize while maintaining aspect ratio
@@ -26,6 +44,7 @@ def process_image(array, threshold, max_size=(1300, 800)):
 
 
 def update_image(value):
+    global threshold
     # Update the image based on the slider's value
     threshold = float(value)
     new_img = process_image(array, threshold)
@@ -67,7 +86,7 @@ def combine_layers(predictions, max_distance):
 
 
 def combine_arrays(directory_path, ignore_percent=0):
-    arrays = []
+    global arrays
 
     paths = [x for x in os.listdir(directory_path) if x.endswith('.npy')]
     paths.sort(key=lambda x: int(x.split('_')[2]))
@@ -79,7 +98,6 @@ def combine_arrays(directory_path, ignore_percent=0):
             array = np.load(file_path)
             arrays.append(array)
 
-    ignore_percent = 0.1
     if ignore_percent > 0:
         print(f"Ignoring {ignore_percent * 100}% of top and bottom layers")
         print("Before:", len(arrays))
@@ -87,19 +105,14 @@ def combine_arrays(directory_path, ignore_percent=0):
         arrays = arrays[ignore_count:-ignore_count]
         print("After:", len(arrays))
 
-    combined_array = combine_layers(arrays, max_distance=2)  # max_distance: how far can layer influence each other
+    combined_array = np.maximum.reduce(arrays)
+    # combined_array = np.sum(arrays, axis=0)
+    # combined_array = combine_layers(arrays, max_distance=2)  # max_distance: how far can layer influence each other
 
     min_value = combined_array.min()
     max_value = combined_array.max()
     normalized_array = (combined_array - min_value) / (max_value - min_value)
     return normalized_array
-    # plt.imshow(normalized_array > 0.2, cmap='gray')
-
-    # Display and save the plot
-    # plt.imshow(combined_array, cmap='gray')  # Change colormap if needed
-    # plt.colorbar()
-    # plt.show()
-    # plt.savefig(save_path)
 
 
 def increase_slider():
@@ -116,10 +129,34 @@ def decrease_slider():
     update_image(new_value)
 
 
+def mode_changed():
+    global array
+    # This function will be called when the mode is changed.
+    # You can use mode_var.get() to get the current mode value.
+    mode = mode_var.get()
+    if mode == 0:
+        combined_array = np.maximum.reduce(arrays)
+        # combined_array = np.sum(arrays, axis=0)
+        # combined_array = combine_layers(arrays, max_distance=2)  # max_distance: how far can layer influence each other
+
+        min_value = combined_array.min()
+        max_value = combined_array.max()
+        normalized_array = (combined_array - min_value) / (max_value - min_value)
+        array = normalized_array
+    elif mode == 1:
+        combined_array = np.sum(arrays, axis=0)
+        min_value = combined_array.min()
+        max_value = combined_array.max()
+        normalized_array = (combined_array - min_value) / (max_value - min_value)
+        array = normalized_array
+    update_image(threshold)
+
+
 if __name__ == "__main__":
-    folder_path = r"A:\handlabel\test\20231123-205540"  # big (330?)
-    # folder_path = r"A:\handlabel\test\20231123-212933"  # small (535)
-    array = combine_arrays(folder_path, ignore_percent=0.2)
+    # folder_path = r"A:\handlabel\test\20231123-205540"  # big (330?)
+    folder_path = r"A:\handlabel\test\20231123-212933"  # small (35)
+    # folder_path = r"A:\handlabel\test\20231125-220804"  # huge 336
+    array = combine_arrays(folder_path, ignore_percent=0.25)
     # print(array.shape)
     # exit()
 
@@ -127,17 +164,35 @@ if __name__ == "__main__":
     root = Tk()
     root.title("Threshold Visualizer")
 
+    # Variable to hold the selected mode
+    mode_var = IntVar(value=0)  # Default to mode 0
+
+    # Create a frame for the mode selection buttons
+    mode_frame = Frame(root)
+    mode_frame.pack(side='top')
+
+    # List of modes
+    modes = ["Sum", "Max"]
+
+    # Create a Radiobutton for each mode
+    for index, mode in enumerate(modes):
+        Radiobutton(mode_frame, text=mode, variable=mode_var, value=index, command=mode_changed).pack(side='left')
+
+    # Create a frame for the slider and buttons
+    control_frame = Frame(root)
+    control_frame.pack(side='top')
+
     # Add left button for decreasing the slider value
-    left_button = Button(root, text="Left", command=decrease_slider)
+    left_button = Button(control_frame, text="  -  ", command=decrease_slider)
     left_button.pack(side='left')
 
     # Add a slider
-    slider = Scale(root, from_=0, to=1, orient=HORIZONTAL, resolution=0.001, length=500, command=update_image)
+    slider = Scale(control_frame, from_=0, to=1, orient=HORIZONTAL, resolution=0.001, length=500, command=update_image)
     slider.set(0.5)  # Set initial value of the slider
     slider.pack(side='left')
 
     # Add right button for increasing the slider value
-    right_button = Button(root, text="Right", command=increase_slider)
+    right_button = Button(control_frame, text="  +  ", command=increase_slider)
     right_button.pack(side='left')
 
     # Display area for the image
