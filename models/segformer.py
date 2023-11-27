9e9e4df9f75ef9b64c3c192208621d092e348844
Marcel Roth <MarcelRoth100@aol.com>
2023-11-27 01:03:32 +0100

    remove redundant code

--- models/segformer.py
+++ models/segformer.py
@@ -6,14 +6,6 @@ from models.abstract_model import AbstractVesuvLightningModule
 class SegformerModule(AbstractVesuvLightningModule):
     def __init__(self, cfg):
         super().__init__(cfg=cfg)
-        # segformer_config = SegformerConfig(
-        #     # hidden_dropout_prob=0.05,
-        #     # drop_path_rate=0.05,
-        #     # attention_probs_dropout_prob=0.05,
-        #     # classifier_dropout_prob=0.05,
-        #     num_labels=1,
-        #     num_channels=cfg.in_chans,
-        # )
 
         self.model = SegformerForSemanticSegmentation.from_pretrained(
             pretrained_model_name_or_path=cfg.from_pretrained,
