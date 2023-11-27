d3848e8e96a5a5693eb7322d52c32991438b240c
Marcel Roth <MarcelRoth100@aol.com>
2023-11-27 13:11:34 +0100

    add

--- configs/datasets/slice/ultra_magnus_slice.py
+++ configs/datasets/slice/ultra_magnus_slice.py
@@ -1,6 +1,6 @@
 
 frag_id = 2
-count = 30_000
+count = 250_000
 slice_length = 64
 slice_width = 1
 slice_depth = 64
--- configs/efficientnetv2/efficientnetv2_s.py
+++ configs/efficientnetv2/efficientnetv2_s.py
@@ -14,13 +14,13 @@ architecture = 'tf_efficientnetv2_s'
 model_name = f"{architecture}-{model_type}"
 from_pretrained = f"tf_efficientnetv2_s"
 in_chans = 1
-seed = 235235
-epochs = 50
+seed = 42
+epochs = 100
 
 num_workers = 4
-train_batch_size = 128
-val_batch_size = 128
+train_batch_size = 2560
+val_batch_size = 2560
 
-lr = 1e-4
-eta_min = 1e-5
-weight_decay = 0.01
+lr = 1e-6
+eta_min = 1e-7
+weight_decay = 0.1
