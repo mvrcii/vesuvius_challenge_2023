ff7ceea747387c4d845445d75e31f0b85acf6da6
Marcel Roth <MarcelRoth100@aol.com>
2023-11-28 18:27:35 +0100

    add segformer b2 256px 4ch

---
+++ configs/segformer/b2_256px_4ch.py
@@ -0,0 +1,24 @@
+import os
+
+_base_ = [
+    "configs/datasets/single/ultra_magnus_256px.py",
+    "configs/schedules/adamw_cosine_lr.py",
+]
+
+work_dir = os.path.join("/scratch", "medfm", "vesuv", "kaggle1stReimp")
+base_label_dir = os.path.join("data", "base_label_files")
+data_root_dir = "data"
+dataset_target_dir = os.path.join("data", "datasets")
+
+model_type = "b2"
+architecture = 'segformer'
+model_name = f"{architecture}-{model_type}"
+from_pretrained = f"nvidia/mit-{model_type}"
+in_chans = 4
+seed = 240783
+epochs = -1
+
+dataset_fraction = 1
+num_workers = 4
+train_batch_size = 2
+val_batch_size = 2
