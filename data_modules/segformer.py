fix augmentation and normalization

data_modules/segformer.py
@@ -6,6 +6,7 @@ import numpy as np
 from lightning.pytorch import LightningDataModule
 from skimage.transform import resize
 from torch.utils.data import Dataset, DataLoader
+from transformers import SegformerImageProcessor
 
 from data.create_dataset import build_dataset_dir_from_config
 
@@ -23,12 +24,29 @@ class SegFormerDataModule(LightningDataModule):
         return self.build_dataloader(dataset_type='val')
 
     def get_transforms(self, dataset_type):
+        image_processor = SegformerImageProcessor.from_pretrained(pretrained_model_name_or_path=self.cfg.from_pretrained)
+
+        mean, std = image_processor.image_mean, image_processor.image_std
+
+        mean.append(np.mean(mean))
+        mean = np.array(mean)
+        assert len(mean) == self.cfg.in_chans
+
+        std.append(np.mean(std))
+        std = np.array(std)
+        assert len(std) == self.cfg.in_chans
+
+        normalize = A.Normalize(mean=mean, std=std)
+
+        transforms = []
         if dataset_type == 'train':
-            return (A.Compose(self.cfg.train_common_aug, is_check_shapes=False),
-                    A.Compose(self.cfg.train_image_aug, is_check_shapes=False))
+            transforms = self.cfg.train_aug
         elif dataset_type == 'val':
-            return (A.Compose(self.cfg.val_common_aug, is_check_shapes=False),
-                    A.Compose(self.cfg.val_image_aug, is_check_shapes=False))
+            transforms = self.cfg.val_aug
+
+        transforms.append(normalize)
+
+        return A.Compose(transforms=transforms, is_check_shapes=False)
 
     def build_dataloader(self, dataset_type):
         data_dir = os.path.join(self.data_root_dir, dataset_type)
@@ -50,16 +68,14 @@ class SegFormerDataModule(LightningDataModule):
         images_list = images_list[:num_samples]
         label_list = label_list[:num_samples]
 
-        # Get transformations
-        common_aug, image_aug = self.get_transforms(dataset_type=dataset_type)
+        transform = self.get_transforms(dataset_type=dataset_type)
 
         dataset = WuesuvDataset(img_dir=img_dir,
                                 label_dir=label_dir,
                                 images=images_list,
                                 labels=label_list,
                                 label_size=self.cfg.label_size,
-                                common_aug=common_aug,
-                                image_aug=image_aug)
+                                transform=transform)
 
         data_loader = DataLoader(dataset,
                                  batch_size=self.cfg.train_batch_size if dataset_type == 'train' else self.cfg.val_batch_size,
@@ -73,15 +89,12 @@ class SegFormerDataModule(LightningDataModule):
 
 
 class WuesuvDataset(Dataset):
-    def __init__(self, img_dir, label_dir, images, label_size, labels=None, common_aug=None, image_aug=None):
+    def __init__(self, img_dir, label_dir, images, label_size, transform, labels=None):
         self.images = np.array(images)
         self.labels = labels
         self.img_dir = img_dir
         self.label_dir = label_dir
-
-        self.common_aug = common_aug
-        self.image_aug = image_aug
-
+        self.transform = transform
         self.label_shape = (label_size, label_size)
 
     def __len__(self):
@@ -98,17 +111,12 @@ class WuesuvDataset(Dataset):
         # Rearrange image from (channels, height, width) to (height, width, channels) to work with albumentations
         image = np.transpose(image, (1, 2, 0))
 
-        # Apply common augmentations to both image and label
-        if self.common_aug:
-            augmented = self.common_aug(image=image, mask=label)
+        # Apply augmentations and normalization
+        if self.transform:
+            augmented = self.transform(image=image, mask=label)
             image, label = augmented['image'], augmented['mask']
 
-        # Apply image-specific augmentations (like resizing)
-        if self.image_aug:
-            augmented = self.image_aug(image=image)
-            image = augmented['image']
-
-        # Rearrange image back to (channels, height, width) from (height, width, channels) to work with segformer input
+        # Rearrange image back from (height, width, channels) to (channels, height, width) to work with segformer input
         image = np.transpose(image, (2, 0, 1))
 
         # Scale down label to match segformer output