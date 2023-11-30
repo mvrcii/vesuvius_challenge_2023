8b459aa312ac84e865b08936fa69465117a63407
Marcel Roth <MarcelRoth100@aol.com>
2023-11-30 14:52:10 +0100

    add labels and negative for bumblebee

--- util/check_dataset_dims.py
+++ util/check_dataset_dims.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 import numpy as np
 
 def is_valid_data(file_path):
@@ -6,7 +8,6 @@ def is_valid_data(file_path):
         data = np.load(file_path, allow_pickle=True)
         # Check if any dimension is zero
         if 0 in data.shape:
-
             return False
         return True
     except Exception as e:
@@ -15,20 +16,29 @@ def is_valid_data(file_path):
 
 def check_dataset(directory):
     invalid_files = []
+    check_count = 0
     for root, dirs, files in os.walk(directory):
         for filename in files:
             if filename.endswith('.npy'):
                 file_path = os.path.join(root, filename)
+                check_count += 1
                 if not is_valid_data(file_path):
                     invalid_files.append(file_path)
-
+    print("Checked", check_count, "files")
     return invalid_files
 
+if len(sys.argv) != 2:
+    print("Usage: python util/check_dataset_dims.py <dataset_root_dir>")
+    sys.exit(1)
+
+dataset_dir = sys.argv[1]
+
 # Directory containing your dataset
-data_directory = 'data/datasets/single/512px/OPTIMUS_STARSCREAM'
+# data_directory = 'data/datasets/single/512px/OPTIMUS_STARSCREAM'
+print("Dataset Directory to check:", dataset_dir)
 
 # Check the dataset
-invalid_data = check_dataset(data_directory)
+invalid_data = check_dataset(dataset_dir)
 print(f"Number of invalid data items: {len(invalid_data)}")
 if invalid_data:
     print("Invalid data files:", invalid_data)
