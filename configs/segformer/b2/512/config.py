cec632c4f9d055a490c4420fb96a6ce23f973fe5
Marcel Roth <MarcelRoth100@aol.com>
2023-12-02 21:20:10 +0100

    add bash scripts

--- configs/segformer/b2/512/config.py
+++ configs/segformer/b2/512/config.py
@@ -42,10 +42,10 @@ model_name = f"{architecture}-{model_type}"
 from_pretrained = f"nvidia/mit-{model_type}"
 # from_checkpoint = "fine-wildflower-497-segformer-b2-231128-164424"
 in_chans = 4
-seed = 52352235
+seed = 187
 epochs = -1
-dataset_fraction = 0.1
+dataset_fraction = 1
 
-num_workers = 4
-train_batch_size = 4
-val_batch_size = 4
+num_workers = 16
+train_batch_size = 24
+val_batch_size = 24
