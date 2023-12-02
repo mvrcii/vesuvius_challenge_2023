40eb689049cfe2ecf52f222e92623e997c6dccae
Marcel Roth <MarcelRoth100@aol.com>
2023-12-02 21:19:11 +0100

    add bash scripts

--- configs/segformer/b2/512/config.py
+++ configs/segformer/b2/512/config.py
@@ -1,6 +1,7 @@
 import os
 import albumentations as A
-from constants import ULTRA_MAGNUS_FRAG_ID
+from constants import (ULTRA_MAGNUS_FRAG_ID, OPTIMUS_FRAG_ID, IRONHIDE_FRAG_ID, MEGATRON_FRAG_ID,
+                       BUMBLEBEE_FRAG_ID, SOUNDWAVE_FRAG_ID, STARSCREAM_FRAG_ID, RATCHET_FRAG_ID)
 
 _base_ = [
     "configs/schedules/adamw_cosine_lr.py",
@@ -17,7 +18,8 @@ label_size = patch_size // 4
 stride = patch_size // 2
 ink_ratio = 3
 artefact_ratio = 5
-fragment_ids = [ULTRA_MAGNUS_FRAG_ID]
+# fragment_ids = [ULTRA_MAGNUS_FRAG_ID]
+fragment_ids = [OPTIMUS_FRAG_ID, BUMBLEBEE_FRAG_ID, MEGATRON_FRAG_ID, STARSCREAM_FRAG_ID, SOUNDWAVE_FRAG_ID, IRONHIDE_FRAG_ID, RATCHET_FRAG_ID]
 train_split = 0.8
 train_aug = [
     A.OneOf([
@@ -42,8 +44,8 @@ from_pretrained = f"nvidia/mit-{model_type}"
 in_chans = 4
 seed = 52352235
 epochs = -1
+dataset_fraction = 0.1
 
-dataset_fraction = 1
-num_workers = 16
-train_batch_size = 24
-val_batch_size = 24
+num_workers = 4
+train_batch_size = 4
+val_batch_size = 4
