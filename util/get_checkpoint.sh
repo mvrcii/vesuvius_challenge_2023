3b9b5a830a18e5001701fe9ba8cf67eae0e86e16
JakeGonder <gonderjake@gmail.com>
2023-12-03 14:34:52 +0100

    fix getcheckpoint

--- util/get_checkpoint.sh
+++ util/get_checkpoint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Check if a folder name is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <checkpoint_folder_name>"
+# Check if two arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <checkpoint_folder_name> <hostname>"
     exit 1
 fi
 
-# Assign the folder name to a variable
+# Assign the folder name and hostname to variables
 FOLDER_NAME=$1
+HOSTNAME=$2
 
-SERVER_PATH="/scratch/medfm/vesuv/kaggle1stReimp/checkpoints"
+# Set the server path based on the hostname
+if [ "$HOSTNAME" = "vast" ]; then
+    SERVER_PATH="~/kaggle1stReimp"
+else
+    SERVER_PATH="/scratch/medfm/vesuv/kaggle1stReimp/checkpoints"
+fi
 
 # Full path on the server
 FULL_SERVER_PATH="$SERVER_PATH/$FOLDER_NAME"
@@ -17,9 +23,8 @@ FULL_SERVER_PATH="$SERVER_PATH/$FOLDER_NAME"
 # Define the local path where you want to save the folder
 LOCAL_PATH="$(pwd)/checkpoints/$FOLDER_NAME"
 
-
 # Using scp to copy the directory. -r flag is used for recursive copy.
-scp -r "slurmmaster-ls6:$FULL_SERVER_PATH" "$LOCAL_PATH"
+scp -r "$HOSTNAME:$FULL_SERVER_PATH" "$LOCAL_PATH"
 
 # Check if scp succeeded
 if [ $? -eq 0 ]; then
@@ -27,4 +32,3 @@ if [ $? -eq 0 ]; then
 else
     echo "Error in copying folder."
 fi
-
