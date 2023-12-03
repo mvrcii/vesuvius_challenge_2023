6bf2e58b3baa0b1e538fea8e678161bd8b40e80a
Marcel Roth <MarcelRoth100@aol.com>
2023-12-03 21:34:32 +0100

    fix

--- util/get_inference.sh
+++ util/get_inference.sh
@@ -19,20 +19,29 @@ elif [ "$HOSTNAME" = "slurm" ]; then
     SERVER_PATH="/scratch/medfm/vesuv/kaggle1stReimp/inference/results"
 else
     echo "Wrong hostname"
+    exit 1
 fi
 
 # Full path on the server
 FULL_SERVER_PATH="$SERVER_PATH/fragment$FRAGMENT_ID/$FOLDER_NAME"
 
 # Define the local path where you want to save the folder
-LOCAL_PATH="$(pwd)/inference/results/fragment$FRAGMENT_ID/$FOLDER_NAME"
+LOCAL_PATH="$(pwd)/inference/results/fragment$FRAGMENT_ID"
+
+# Check if the directory exists, create it if not
+if [ ! -d "$LOCAL_PATH" ]; then
+    mkdir -p "$LOCAL_PATH"
+fi
 
 # Using scp to copy the directory. -r flag is used for recursive copy.
 scp -r "$HOSTNAME:$FULL_SERVER_PATH" "$LOCAL_PATH"
+
+read -p "Press Enter to Continue"
 
 # Check if scp succeeded
 if [ $? -eq 0 ]; then
     echo "Folder copied successfully."
 else
     echo "Error in copying folder."
+    exit 1
 fi
