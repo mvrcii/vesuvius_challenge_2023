b033b0bdefbb1605d9787825b3e91b7fd5b66662
JakeGonder <gonderjake@gmail.com>
2023-12-03 14:45:46 +0100

    add ps get checkpiont

---
+++ util/get_checkpoint.ps1
@@ -0,0 +1,32 @@
+# Check if two arguments are provided
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$FolderName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Hostname
+)
+
+# Set the server path based on the hostname
+if ($Hostname -eq "vast") {
+    $ServerPath = "~/kaggle1stReimp"
+} else {
+    $ServerPath = "/scratch/medfm/vesuv/kaggle1stReimp/checkpoints"
+}
+
+# Full path on the server
+$FullServerPath = "$ServerPath/$FolderName"
+
+# Define the local path where you want to save the folder
+$LocalPath = Join-Path -Path (Get-Location) -ChildPath "checkpoints\$FolderName"
+
+# Using scp to copy the directory. SCP needs to be installed on Windows, typically via OpenSSH
+$ScpCommand = "scp -r $Hostname:`"$FullServerPath`" $LocalPath"
+Invoke-Expression $ScpCommand
+
+# Check if scp succeeded
+if ($LastExitCode -eq 0) {
+    Write-Host "Folder copied successfully."
+} else {
+    Write-Host "Error in copying folder."
+}
