ad529bbe1896bc09b61a593b83192cb8eb94cf6f
Marcel Roth <MarcelRoth100@aol.com>
2023-12-04 13:08:12 +0100

    fix plot scheduler

--- util/plot_scheduler.py
+++ util/plot_scheduler.py
@@ -6,15 +6,15 @@ import matplotlib.pyplot as plt
 STEPS = 100
 
 lr = 2e-4
-weight_decay = 0.01
+weight_decay = 0.005
 
 optimizer = AdamW([torch.tensor(1)], lr=lr, weight_decay=weight_decay)
 # Use a scheduler of your choice below.
 # Great for debugging your own schedulers!
 scheduler = StepLR(
     optimizer,
-    step_size=2,
-    gamma=0.99
+    step_size=10,
+    gamma=0.8
 )
 
 # scheduler = CosineAnnealingLR(optimizer, STEPS)
@@ -22,8 +22,9 @@ scheduler = StepLR(
 lrs = []
 for _ in range(500):
     optimizer.step()
-    lrs.append(scheduler.get_lr())
+    lrs.append(scheduler.get_last_lr())
     scheduler.step()
 
+print(lrs)
 plt.plot(lrs)
 plt.show()
