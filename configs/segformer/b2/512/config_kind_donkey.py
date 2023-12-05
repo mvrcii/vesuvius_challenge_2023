48581357ed88ab9cea71fd2a6201d3468f5f951d
Marcel Roth <MarcelRoth100@aol.com>
2023-12-05 18:40:31 +0100

    update config

--- configs/segformer/b2/512/config_kind_donkey.py
+++ configs/segformer/b2/512/config_kind_donkey.py
@@ -1,5 +1,7 @@
 import os
+
 import albumentations as A
+
 from constants import (ULTRA_MAGNUS_FRAG_ID, OPTIMUS_FRAG_ID, IRONHIDE_FRAG_ID, MEGATRON_FRAG_ID,
                        BUMBLEBEE_FRAG_ID, SOUNDWAVE_FRAG_ID, STARSCREAM_FRAG_ID, RATCHET_FRAG_ID)
 
@@ -27,12 +29,9 @@ train_aug = [
         A.VerticalFlip(),
         A.RandomRotate90(),
         A.Transpose(),
-        # A.RandomScale(scale_limit=0.1, p=0.5),
-        # A.CenterCrop(height=size, width=size, p=0.5),
         A.RandomGamma(always_apply=True, gamma_limit=(56, 150), eps=None),
         A.AdvancedBlur(always_apply=True, blur_limit=(3, 5), sigmaX_limit=(0.2, 1.0), sigmaY_limit=(0.2, 1.0),
                        rotate_limit=(-90, 90), beta_limit=(0.5, 8.0), noise_limit=(0.9, 1.1)),
-        A.CLAHE(always_apply=True, clip_limit=(1, 4), tile_grid_size=(8, 8)),
         A.ChannelDropout(always_apply=True, channel_drop_range=(1, 1), fill_value=0),
         A.CoarseDropout(always_apply=True, max_holes=6, max_height=56, max_width=56, min_holes=2, min_height=38,
                         min_width=38, fill_value=0, mask_fill_value=None),
@@ -48,7 +47,6 @@ train_aug = [
                             ratio=(0.75, 1.51),
                             interpolation=0)
     ], p=0.5),
-
 ]
 val_aug = [
 ]
@@ -62,8 +60,13 @@ from_pretrained = f"nvidia/mit-{model_type}"
 in_chans = 4
 seed = 7777
 epochs = -1
-loss = "dice+bce"
+losses = [("bce", 1.0), ("dice", 1.0)]
 dataset_fraction = 1
+
+val_interval = 1
+lr = 2e-4
+step_lr_steps = 2
+step_lr_factor = 0.99
 
 num_workers = 16
 train_batch_size = 24
