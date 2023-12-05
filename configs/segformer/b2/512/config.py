d15f164f4de36e462614693529125751ef43a13a
Marcel Roth <MarcelRoth100@aol.com>
2023-12-05 20:42:50 +0100

    refactors current config

--- configs/segformer/b2/512/config.py
+++ configs/segformer/b2/512/config.py
@@ -16,15 +16,11 @@ dataset_target_dir = os.path.join("data", "datasets")
 patch_size = 512
 label_size = patch_size // 4
 stride = patch_size // 2
-ink_ratio = 3
-artefact_ratio = 5
+ink_threshold = 3
+artefact_threshold = 5
 fragment_ids = [ULTRA_MAGNUS_FRAG_ID, OPTIMUS_FRAG_ID, BUMBLEBEE_FRAG_ID, MEGATRON_FRAG_ID, STARSCREAM_FRAG_ID,
                 SOUNDWAVE_FRAG_ID, IRONHIDE_FRAG_ID, RATCHET_FRAG_ID]
 train_split = 0.8
-
-lr = 2e-4
-step_lr_steps = 2
-step_lr_factor = 0.95
 
 # training parameters
 model_type = "b2"
@@ -37,26 +33,28 @@ seed = 5453
 epochs = -1
 losses = [("bce", 1.0), ("dice", 1.0)]
 dataset_fraction = 1
-val_interval = 2
 
-num_workers = 16
-train_batch_size = 24
-val_batch_size = 24
+val_interval = 1
+lr = 2e-4
+step_lr_steps = 2
+step_lr_factor = 0.95
+weight_decay = 0.01
+
+num_workers = 2
+train_batch_size = 4
+val_batch_size = 4
 
 # TRAIN AUG AND VAL AUG HAVE TO BE LAST PARAMETERS OF CONFIG IN THIS ORDER
 train_aug = [
-    A.HorizontalFlip(p=0.5),
-    A.VerticalFlip(p=0.5),
-    A.RandomRotate90(p=0.5),
-    A.Transpose(p=0.5),
-    A.RandomResizedCrop(p=0.5, height=patch_size, width=patch_size, scale=(0.78, 1.0),
-                        ratio=(0.75, 1.51),
-                        interpolation=0),
-    A.ElasticTransform(always_apply=False, p=0.3, alpha=0.59, sigma=10.34, alpha_affine=9.66, interpolation=0, border_mode=0, value=(0, 0, 0), mask_value=None, approximate=False, same_dxdy=False),
     A.OneOf([
+        A.HorizontalFlip(),
+        A.VerticalFlip(),
+        A.RandomRotate90(),
+        A.Transpose(),
         A.RandomGamma(always_apply=True, gamma_limit=(56, 150), eps=None),
         A.AdvancedBlur(always_apply=True, blur_limit=(3, 5), sigmaX_limit=(0.2, 1.0), sigmaY_limit=(0.2, 1.0),
                        rotate_limit=(-90, 90), beta_limit=(0.5, 8.0), noise_limit=(0.9, 1.1)),
+        A.ChannelDropout(always_apply=True, channel_drop_range=(1, 1), fill_value=0),
         A.CoarseDropout(always_apply=True, max_holes=6, max_height=56, max_width=56, min_holes=2, min_height=38,
                         min_width=38, fill_value=0, mask_fill_value=None),
         A.Downscale(always_apply=True, scale_min=0.55, scale_max=0.99),
@@ -64,6 +62,12 @@ train_aug = [
                          border_mode=0,
                          value=(0, 0, 0), mask_value=None, normalized=False),
         A.ImageCompression(always_apply=True, quality_lower=62, quality_upper=91, compression_type=1),
+        A.PiecewiseAffine(always_apply=True, scale=(0.03, 0.03), nb_rows=(3, 3), nb_cols=(3, 3), interpolation=0,
+                          mask_interpolation=0, cval=0, cval_mask=0, mode='constant', absolute_scale=False,
+                          keypoints_threshold=0.01),
+        A.RandomResizedCrop(always_apply=True, height=patch_size, width=patch_size, scale=(0.78, 1.0),
+                            ratio=(0.75, 1.51),
+                            interpolation=0)
     ], p=0.5),
     A.ChannelDropout(p=0.1, channel_drop_range=(1, 1), fill_value=0),
 ]
