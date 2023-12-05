86c780ef2be2a0cd2524a717e2f5633991ed1041
JakeGonder <gonderjake@gmail.com>
2023-12-05 09:52:29 +0100

    update config b1

--- configs/segformer/b1/512/config.py
+++ configs/segformer/b1/512/config.py
@@ -1,6 +1,7 @@
 import os
 import albumentations as A
-from constants import ULTRA_MAGNUS_FRAG_ID
+from constants import (ULTRA_MAGNUS_FRAG_ID, OPTIMUS_FRAG_ID, IRONHIDE_FRAG_ID, MEGATRON_FRAG_ID,
+                       BUMBLEBEE_FRAG_ID, SOUNDWAVE_FRAG_ID, STARSCREAM_FRAG_ID, RATCHET_FRAG_ID)
 
 _base_ = [
     "configs/schedules/adamw_cosine_lr.py",
@@ -15,33 +16,55 @@ dataset_target_dir = os.path.join("data", "datasets")
 patch_size = 512
 label_size = patch_size // 4
 stride = patch_size // 2
-ink_ratio = 0.05
-fragment_ids = [ULTRA_MAGNUS_FRAG_ID]
+ink_ratio = 3
+artefact_ratio = 5
+fragment_ids = [ULTRA_MAGNUS_FRAG_ID, OPTIMUS_FRAG_ID, BUMBLEBEE_FRAG_ID, MEGATRON_FRAG_ID, STARSCREAM_FRAG_ID,
+                SOUNDWAVE_FRAG_ID, IRONHIDE_FRAG_ID, RATCHET_FRAG_ID]
 train_split = 0.8
 train_aug = [
+    A.HorizontalFlip(p=0.5),
+    A.VerticalFlip(p=0.5),
+    A.RandomRotate90(p=0.5),
+    A.Transpose(p=0.5),
+    A.RandomResizedCrop(p=0.5, height=patch_size, width=patch_size, scale=(0.78, 1.0),
+                        ratio=(0.75, 1.51),
+                        interpolation=0),
+    A.ElasticTransform(always_apply=False, p=0.3, alpha=0.59, sigma=10.34, alpha_affine=9.66, interpolation=0, border_mode=0, value=(0, 0, 0), mask_value=None, approximate=False, same_dxdy=False),
     A.OneOf([
-        A.HorizontalFlip(),
-        A.VerticalFlip(),
-        A.RandomRotate90(),
-        A.Transpose(),
+        A.RandomGamma(always_apply=True, gamma_limit=(56, 150), eps=None),
+        A.AdvancedBlur(always_apply=True, blur_limit=(3, 5), sigmaX_limit=(0.2, 1.0), sigmaY_limit=(0.2, 1.0),
+                       rotate_limit=(-90, 90), beta_limit=(0.5, 8.0), noise_limit=(0.9, 1.1)),
+        A.CoarseDropout(always_apply=True, max_holes=6, max_height=56, max_width=56, min_holes=2, min_height=38,
+                        min_width=38, fill_value=0, mask_fill_value=None),
+        A.Downscale(always_apply=True, scale_min=0.55, scale_max=0.99),
+        A.GridDistortion(always_apply=True, num_steps=15, distort_limit=(-0.19, 0.19), interpolation=0,
+                         border_mode=0,
+                         value=(0, 0, 0), mask_value=None, normalized=False),
+        A.ImageCompression(always_apply=True, quality_lower=62, quality_upper=91, compression_type=1),
     ], p=0.5),
-    A.RandomResizedCrop(height=patch_size, width=patch_size, scale=(0.4, 0.9), ratio=(1, 1), p=0.5),
-    # A.RandomScale(scale_limit=0.1, p=0.5),
-    # A.CenterCrop(height=size, width=size, p=0.5),
+    A.ChannelDropout(p=0.1, channel_drop_range=(1, 1), fill_value=0),
 ]
 val_aug = [
 ]
+
+lr = 2e-4
+step_lr_steps = 2
+step_lr_factor = 0.95
 
 # training parameters
 model_type = "b1"
 architecture = 'segformer'
 model_name = f"{architecture}-{model_type}"
 from_pretrained = f"nvidia/mit-{model_type}"
+# from_checkpoint = "kind-donkey-583-segformer-b2-231204-001337"
 in_chans = 4
-seed = 187
-epochs = 150
-
+seed = 5453
+epochs = -1
+losses = [("bce", 1.0), ("dice", 1.0)]
 dataset_fraction = 1
-num_workers = 4
+val_interval = 2
+
+num_workers = 16
 train_batch_size = 24
 val_batch_size = 24
+
