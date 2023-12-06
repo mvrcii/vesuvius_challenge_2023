a690a32746f8b6f61256de3d060496a463dc9b95
JakeGonder <gonderjake@gmail.com>
2023-12-06 16:59:47 +0100

    add layer shift and dropout augmentation

--- models/inception_backbone.py
+++ models/inception_backbone.py
@@ -10,6 +10,29 @@ from torch.optim import AdamW
 from warmup_scheduler import GradualWarmupScheduler
 
 
+def layer_shift_and_dropout(self, image):
+    image_tmp = np.zeros_like(image)
+    cropping_num = random.randint(22, 30)
+
+    start_idx = random.randint(0, self.cfg.in_chans - cropping_num)
+    crop_indices = np.arange(start_idx, start_idx + cropping_num)
+
+    start_paste_idx = random.randint(0, self.cfg.in_chans - cropping_num)
+
+    tmp = np.arange(start_paste_idx, cropping_num)
+    np.random.shuffle(tmp)
+
+    cutout_idx = random.randint(0, 2)
+    temporal_random_cutout_idx = tmp[:cutout_idx]
+
+    image_tmp[..., start_paste_idx: start_paste_idx + cropping_num] = image[..., crop_indices]
+
+    if random.random() > 0.4:
+        image_tmp[..., temporal_random_cutout_idx] = 0
+    image = image_tmp
+    return image
+
+
 class CFG:
     # ============== comp exp name =============
     comp_name = 'vesuvius'
