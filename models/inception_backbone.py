f4cd312510ac4881734a4d63b0518ecaa412828c
JakeGonder <gonderjake@gmail.com>
2023-12-06 16:46:02 +0100

    update configs

--- models/inception_backbone.py
+++ models/inception_backbone.py
@@ -14,6 +14,119 @@ import sys
 from collections import OrderedDict
 
 
+class CFG:
+    # ============== comp exp name =============
+    comp_name = 'vesuvius'
+
+    # comp_dir_path = './'
+    comp_dir_path = './'
+    comp_folder_name = './'
+    # comp_dataset_path = f'{comp_dir_path}datasets/{comp_folder_name}/'
+    comp_dataset_path = f'./'
+
+    exp_name = 'pretraining_all'
+
+    # ============== pred target =============
+    target_size = 1
+
+    # ============== model cfg =============
+    model_name = 'Unet'
+    # backbone = 'efficientnet-b0'
+    # backbone = 'se_resnext50_32x4d'
+    backbone = 'resnet3d'
+    in_chans = 30  # 65
+    encoder_depth = 5
+    # ============== training cfg =============
+    size = 64
+    tile_size = 256
+    stride = tile_size // 8
+
+    # todo, original 256
+    train_batch_size = 8  # 32
+    valid_batch_size = train_batch_size
+    use_amp = True
+
+    scheduler = 'GradualWarmupSchedulerV2'
+    # scheduler = 'CosineAnnealingLR'
+    epochs = 30  # 30
+
+    # adamW warmupあり
+    warmup_factor = 10
+    # lr = 1e-4 / warmup_factor
+    # lr = 1e-4 / warmup_factor
+    lr = 2e-5
+    # ============== fold =============
+    valid_id = '20230827161847'
+
+    # objective_cv = 'binary'  # 'binary', 'multiclass', 'regression'
+    metric_direction = 'maximize'  # maximize, 'minimize'
+    # metrics = 'dice_coef'
+
+    # ============== fixed =============
+    pretrained = True
+    inf_weight = 'best'  # 'best'
+
+    min_lr = 1e-6
+    weight_decay = 1e-6
+    max_grad_norm = 100
+
+    print_freq = 50
+    num_workers = 1
+
+    seed = 0
+
+    # ============== set dataset path =============
+    print('set dataset path')
+
+    outputs_path = f'./outputs/{comp_name}/{exp_name}/'
+
+    submission_dir = outputs_path + 'submissions/'
+    submission_path = submission_dir + f'submission_{exp_name}.csv'
+
+    model_dir = outputs_path + \
+                f'{comp_name}-models/'
+
+    figures_dir = outputs_path + 'figures/'
+
+    log_dir = outputs_path + 'logs/'
+    log_path = log_dir + f'{exp_name}.txt'
+
+    # ============== augmentation =============
+    train_aug_list = [
+        # A.RandomResizedCrop(
+        #     size, size, scale=(0.85, 1.0)),
+        A.Resize(size, size),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        # A.RandomRotate90(p=0.6),
+
+        A.RandomBrightnessContrast(p=0.75),
+        A.ShiftScaleRotate(rotate_limit=360, shift_limit=0.15, scale_limit=0.15, p=0.75),
+        A.OneOf([
+            A.GaussNoise(var_limit=[10, 50]),
+            A.GaussianBlur(),
+            A.MotionBlur(),
+        ], p=0.4),
+        # A.GridDistortion(num_steps=5, distort_limit=0.3, p=0.5),
+        A.CoarseDropout(max_holes=2, max_width=int(size * 0.2), max_height=int(size * 0.2),
+                        mask_fill_value=0, p=0.5),
+        # A.Cutout(max_h_size=int(size * 0.6),
+        #          max_w_size=int(size * 0.6), num_holes=1, p=1.0),
+        A.Normalize(
+            mean=[0] * in_chans,
+            std=[1] * in_chans
+        ),
+        ToTensorV2(transpose_mask=True),
+    ]
+
+    valid_aug_list = [
+        A.Resize(size, size),
+        A.Normalize(
+            mean=[0] * in_chans,
+            std=[1] * in_chans
+        ),
+        ToTensorV2(transpose_mask=True),
+    ]
 class MaxPool3dSamePadding(nn.MaxPool3d):
 
     def compute_pad(self, dim, s):
