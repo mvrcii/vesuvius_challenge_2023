a6383e650ba03886e918bc489371411a7860abbc
Marcel Roth <MarcelRoth100@aol.com>
2023-12-06 23:55:08 +0100

    FIX

--- batch_infer_layered.py
+++ batch_infer_layered.py
@@ -7,7 +7,7 @@ import albumentations as A
 import cv2
 import numpy as np
 import torch
-from PIL.Image import Image
+from PIL import Image
 from tqdm import tqdm
 from transformers import SegformerForSemanticSegmentation
 
