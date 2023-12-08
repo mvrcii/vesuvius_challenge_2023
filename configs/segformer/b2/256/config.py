11dc0cd151bde665ee9f18fa94028fd45e43e816
Marcel Roth <MarcelRoth100@aol.com>
2023-12-08 18:15:03 +0100

    update config 256

--- configs/segformer/b2/256/config.py
+++ configs/segformer/b2/256/config.py
@@ -44,8 +44,8 @@ step_lr_factor = 0.97
 weight_decay = 0.01
 
 num_workers = 16
-train_batch_size = 128
-val_batch_size = 128
+train_batch_size = 48
+val_batch_size = 48
 
 # TRAIN AUG AND VAL AUG HAVE TO BE LAST PARAMETERS OF CONFIG IN THIS ORDER
 train_aug = [
