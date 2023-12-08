38c13e3381e41b7889abda61da88f3cf55b42ef4
JakeGonder <gonderjake@gmail.com>
2023-12-08 16:09:00 +0100

    update ensemble

--- inference/ensemble.py
+++ inference/ensemble.py
@@ -63,6 +63,7 @@ def main(fragment__id, strategy):
             npy_files.append(np.load(npy_path))
 
         # IMPLEMENT DIFFERENT STRATEGIES HERE
+        result = None
         if strategy == "mean":
             result = np.mean(np.stack(npy_files), axis=0)
         elif strategy == "max":
