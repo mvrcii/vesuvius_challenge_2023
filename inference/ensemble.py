d1042eb1627c7ae860fbe53142d13674ccfdf46b
JakeGonder <gonderjake@gmail.com>
2023-12-08 15:33:21 +0100

    update ensemble

--- inference/ensemble.py
+++ inference/ensemble.py
@@ -10,47 +10,49 @@ def main(fragment__id, strategy):
     # Check for existing inference directories
     fragment_dir_path = os.path.join("inference", "results", f"fragment{fragment__id}")
     print("checking ", fragment_dir_path)
-    contained_dirs = os.listdir(fragment_dir_path)
-    print("found")
-    print(contained_dirs)
-    inference_dirs = [os.path.join(fragment_dir_path, x) for x in contained_dirs if
-                      os.path.isdir(os.path.join(fragment_dir_path, x)) and not x.startswith("ensemble")]
+
+    contained_dir_paths = [os.path.join(fragment_dir_path, x) for x in os.listdir(fragment_dir_path) if os.path.isdir(
+        os.path.join(fragment_dir_path, x))]
+
+    inference_dir_paths = [x for x in contained_dir_paths if not x.startswith("ensemble")]
 
     # Check if enough directories exist
-    if len(inference_dirs) < 2:
-        print(f"{fragment_dir_path} only contains {len(inference_dirs)} inferences, ensemble not possible.")
+    if len(inference_dir_paths) < 2:
+        print(f"{fragment_dir_path} only contains {len(inference_dir_paths)} inferences, ensemble not possible.")
         exit()
 
     # Determine name of output directory, e.g. ensemble1
-    ensemble_dirs = [x for x in contained_dirs if os.path.isdir(x) and x.startswith("ensemble")]
-    current_ensemble_ids = [int(dir_name.split("_")[-1]) for dir_name in ensemble_dirs]
+    ensemble_dir_paths = [x for x in contained_dir_paths if x.startswith("ensemble")]
+    current_ensemble_ids = [int(os.path.basename(dir_name).split("_")[-1]) for dir_name in ensemble_dir_paths]
     next_ensemble_id = 0
     if len(current_ensemble_ids) > 0:
         next_ensemble_id = max(current_ensemble_ids) + 1
 
     # Ensemble short info id
-    ensemble_info_id = "".join([dir_name[0] for dir_name in inference_dirs]) + f"_{strategy}"
+    ensemble_info_id = "".join([os.path.basename(dir_name)[0] for dir_name in ensemble_dir_paths]) + f"_{strategy}"
+    print("info id: ", ensemble_info_id)
 
     # Output path
     out_path = os.path.join(fragment_dir_path, f"ensemble_{ensemble_info_id}_{next_ensemble_id}")
     os.makedirs(out_path)
+    print("Outpath: ", out_path)
 
     # Collect all .npy file names from each directory
-    all_npy_files = {d: set([f for f in os.listdir(d) if f.endswith('.npy')]) for d in inference_dirs}
+    all_npy_files = {d: set([f for f in os.listdir(d) if f.endswith('.npy')]) for d in inference_dir_paths}
 
     # Find common files across all directories
     common_npy_files = set.intersection(*all_npy_files.values())
 
     # Find and print missing files in each directory
-    for d in inference_dirs:
+    for d in inference_dir_paths:
         missing_files = common_npy_files - all_npy_files[d]
         for file in missing_files:
             print(f"Skipping {file} as it is not contained in {d}")
 
     for npy_name in tqdm(common_npy_files):
         npy_files = []
-        for inference_dir in inference_dirs:
-            npy_path = os.path.join(fragment_dir_path, inference_dir, npy_name)
+        for inference_dir in inference_dir_paths:
+            npy_path = os.path.join(inference_dir, npy_name)
             npy_files.append(np.load(npy_path))
 
         # IMPLEMENT DIFFERENT STRATEGIES HERE
@@ -58,9 +60,9 @@ def main(fragment__id, strategy):
         np.save(os.path.join(out_path, npy_name), result)
 
     # Save config to output path, listing current time as well as the used inference directories
-    config = f"Ensemble of {len(inference_dirs)} inferences, strategy: {strategy}\n"
+    config = f"Ensemble of {len(ensemble_dir_paths)} inferences, strategy: {strategy}\n"
     config += f"Used inferences:\n"
-    for inference_dir in inference_dirs:
+    for inference_dir in ensemble_dir_paths:
         config += f"{inference_dir}\n"
     config += f"Created at {datetime.now()}"
     with open(os.path.join(out_path, "config.txt"), "w") as f:
