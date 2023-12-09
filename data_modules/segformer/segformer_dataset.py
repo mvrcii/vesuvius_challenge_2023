4d9882cab424db7ca607d5b1f0e6f8b48b6241f1
JakeGonder <gonderjake@gmail.com>
2023-12-09 15:37:30 +0100

    fix patchsize loading

--- data_modules/segformer/segformer_dataset.py
+++ data_modules/segformer/segformer_dataset.py
@@ -6,4 +6,9 @@ from data_modules.abstract.abstract_dataset import AbstractDataset
 
 class SegFormerDataset(AbstractDataset):
     def __init__(self, root_dir, images, label_size, patch_size, transform, labels=None):
-        super().__init__(root_dir, images, transform, labels, label_size, patch_size)
+        super().__init__(root_dir=root_dir,
+                         images=images,
+                         transform=transform,
+                         labels=labels,
+                         label_size=label_size,
+                         patch_size=patch_size)
