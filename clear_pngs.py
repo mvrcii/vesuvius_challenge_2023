f221993f5e872b7a97f132d4651b284b50baeedf
JakeGonder <gonderjake@gmail.com>
2023-12-09 04:05:59 +0100

    add clear_pngs

--- clear_pngs.py
+++ clear_pngs.py
@@ -2,16 +2,17 @@ import os
 
 inf_dir = os.path.join("inference", "results")
 valid_start_idxs = [x for x in range(0, 61, 4)]
-for f in os.listdir(inf_dir):
-    for sub_dir in os.listdir(os.path.join(inf_dir, f)):
-        if sub_dir.__contains__("lively-meadow-695"):
-            print(sub_dir, "contains lively-meadow-695")
-            sub_sub_dir = os.listdir(sub_dir)[0]
-            print(sub_sub_dir, "is sub_sub_dir")
-            for file in os.listdir(os.path.join(inf_dir, f, sub_dir, sub_sub_dir, "labels")):
+for fragment_dir in os.listdir(inf_dir):
+    print("Checking", fragment_dir)
+    for run_dir in os.listdir(os.path.join(inf_dir, fragment_dir)):
+        if run_dir.__contains__("lively-meadow-695"):
+            print(run_dir, "contains lively-meadow-695")
+            label_dir = [x for x in os.listdir(run_dir) if x.__contains__("labels")][0]
+            label_sub_dir = os.listdir(os.path.join(inf_dir, fragment_dir, run_dir, label_dir))[0]
+            for file in os.listdir(os.path.join(inf_dir, fragment_dir, run_dir, label_dir, label_sub_dir)):
                 print(file)
                 start_idx = int(file.split("_")[2])
                 if not valid_start_idxs.__contains__(start_idx):
                     print(f"Removing {file}")
-                    print("in " + os.path.join(inf_dir, f, sub_dir, sub_sub_dir, "labels"))
+                    print("in " + os.path.join(inf_dir, fragment_dir, run_dir, label_dir, label_sub_dir))
                     # os.remove(os.path.join(inf_dir, f, sub_dir, sub_sub_dir, "labels", file))
