2965925aae759c96e669c545426eb0c2684a7a2d
JakeGonder <gonderjake@gmail.com>
2023-12-09 04:00:46 +0100

    add clear_pngs

--- clear_pngs.py
+++ clear_pngs.py
@@ -5,7 +5,9 @@ valid_start_idxs = [x for x in range(0, 61, 4)]
 for f in os.listdir(inf_dir):
     for sub_dir in os.listdir(os.path.join(inf_dir, f)):
         if sub_dir.__contains__("lively-meadow-695"):
+            print(sub_dir, "contains lively-meadow-695")
             sub_sub_dir = os.listdir(sub_dir)[0]
+            print(sub_sub_dir, "is sub_sub_dir")
             for file in os.listdir(os.path.join(inf_dir, f, sub_dir, sub_sub_dir, "labels")):
                 print(file)
                 start_idx = int(file.split("_")[2])
