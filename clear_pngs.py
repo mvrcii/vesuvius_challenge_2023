fcf05fa03c1003e6cbccef842d9fa46e3d7f93fa
JakeGonder <gonderjake@gmail.com>
2023-12-09 03:54:47 +0100

    add clear_pngs

--- clear_pngs.py
+++ clear_pngs.py
@@ -4,7 +4,7 @@ inf_dir = os.path.join("inference", "results")
 valid_start_idxs = [x for x in range(0, 61, 4)]
 for f in os.listdir(inf_dir):
     for sub_dir in os.listdir(os.path.join(inf_dir, f)):
-        if sub_dir.__contains__("lively_meadow-695"):
+        if sub_dir.__contains__("lively-meadow-695"):
             for file in os.listdir(os.path.join(inf_dir, f, sub_dir)):
                 start_idx = int(file.split("_")[2])
                 if not valid_start_idxs.__contains__(start_idx):
