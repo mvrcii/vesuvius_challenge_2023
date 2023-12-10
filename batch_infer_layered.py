6f40e2447214cd2aa592a744b6bcb9cfa5bb4038
JakeGonder <gonderjake@gmail.com>
2023-12-10 15:10:26 +0100

    adjust batch_infer

--- batch_infer_layered.py
+++ batch_infer_layered.py
@@ -15,13 +15,13 @@ def print_colored(message, color):
     print(f"{colors[color]}{message}{colors['end']}")
 
 
-FRAGMENT_IDS = [BLASTER_FRAG_ID, HOT_ROD_FRAG_ID, JAZZILLA_FRAG_ID]
-CHECKPOINTS = ["playful-firefly-737-segformer-b2-231209-143850"]
+FRAGMENT_IDS = FRAGMENTS_BETA
+CHECKPOINTS = ["deft-yogurt-738-segformer-b2-231209-215717"]
 
 start_idx = 0
 end_idx = 60
 batch_size = 32
-labels = False
+labels = True
 verbose = False
 
 
