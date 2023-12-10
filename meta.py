a73a0409aa85ac5b882aba7e448b799a3a62b37d
Marcel Roth <MarcelRoth100@aol.com>
2023-12-10 19:00:48 +0100

    add label files

--- meta.py
+++ meta.py
@@ -1,6 +1,7 @@
 import os
 
-from constants import FRAGMENTS_ALPHA, FRAGMENTS_BETA, ITERATION, LABEL_TYPE, ALPHA, BETA, IT_2_MODEL
+from constants import FRAGMENTS_ALPHA, FRAGMENTS_BETA, ITERATION, LABEL_TYPE, ALPHA, BETA, IT_2_MODEL, LABEL_BASE_PATH, \
+    LABEL_TARGET_PATH
 
 
 class AlphaBetaMeta:
@@ -31,14 +32,19 @@ class AlphaBetaMeta:
 
     def get_current_label_dir(self):
         """The current label dir is always specified by the model prior to this iteration."""
-        base_path = "./data/base_label_files/"
         label_type = self.get_current_label_type()
         model = self.get_previous_model()
 
         if model:
-            return os.path.join(base_path, label_type, model)
+            return os.path.join(LABEL_BASE_PATH, label_type, model)
         else:
             raise Exception(f"No prior model for iteration {self.iteration} ({self.get_current_phase()}) found")
+
+    def get_label_base_dir(self):
+        return os.path.join(LABEL_BASE_PATH, self.get_current_label_type())
+
+    def get_label_target_dir(self):
+        return os.path.join(LABEL_TARGET_PATH, self.get_current_label_type())
 
     def get_current_model(self):
         return self.get_model_for_it(self.iteration)
@@ -46,11 +52,20 @@ class AlphaBetaMeta:
     def get_previous_model(self):
         return self.get_model_for_it(self.iteration - 1)
 
-    def get_current_fragments(self):
+    def get_current_train_fragments(self):
         phase = self.get_current_phase()
         if phase == ALPHA:
             return list(FRAGMENTS_ALPHA)
         elif phase == BETA:
             return list(FRAGMENTS_BETA)
+        else:
+            raise Exception("No such current phase:", phase)
+
+    def get_current_inference_fragments(self):
+        phase = self.get_current_phase()
+        if phase == ALPHA:
+            return list(FRAGMENTS_BETA)
+        elif phase == BETA:
+            return list(FRAGMENTS_ALPHA)
         else:
             raise Exception("No such current phase:", phase)
