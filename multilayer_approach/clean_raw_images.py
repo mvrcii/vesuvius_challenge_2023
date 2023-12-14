2b59fd9b444d0f97158aa1af8a74daa21fcaa5b5
Marcel Roth <MarcelRoth100@aol.com>
2023-12-15 00:09:25 +0100

    renaming

--- multilayer_approach/clean_raw_images.py
+++ multilayer_approach/clean_raw_images.py
@@ -11,13 +11,13 @@ Image.MAX_IMAGE_PIXELS = None
 
 # Load the image
 # path to current directory
-path = "base_labels/0_raw"
-for file in tqdm(os.listdir(path)):
-    os.makedirs(os.path.join(path, 'cleaned'), exist_ok=True)
-
+base_path = "base_labels"
+source_path = os.path.join(base_path, '0_raw')
+target_Path = os.path.join(base_path, '1_cleaned')
+for file in tqdm(os.listdir(source_path)):
     if not file.endswith('.png'):
         continue
-    image = np.asarray(Image.open(os.path.join(path, file)))
+    image = np.asarray(Image.open(os.path.join(source_path, file)))
     _, image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
 
     # Threshold to get a binary image
@@ -28,10 +28,12 @@ for file in tqdm(os.listdir(path)):
     # assert image only has 0 and 255 values
     assert np.array_equal(np.unique(cleaned), np.array([0, 255]))
 
+    os.makedirs(target_Path, exist_ok=True)
     # Save the cleaned image
-    save_path = os.path.join(path, "cleaned", file)
+    save_path = os.path.join(target_Path, file)
     cv2.imwrite(save_path, cleaned)
-print("Done cleaning images in base_labels/0_raw")
+
+print("Done cleaning images in", source_path)
 
 
 
