e07929b6e441462f0fe1859dd4559eb454c05989
Marcel Roth <MarcelRoth100@aol.com>
2023-12-16 17:20:44 +0100

    updates debug model

--- multilayer_approach/debug_model_new.py
+++ multilayer_approach/debug_model_new.py
@@ -4,9 +4,9 @@ import torch
 from skimage.transform import resize
 from torch import float16
 from torch.nn import BCELoss
-from tqdm import tqdm
 
 from config_handler import Config
+from focal_loss import FocalLoss2d
 from models.architectures.unetr_segformer import UNETR_Segformer
 
 
@@ -28,7 +28,7 @@ def calculate_masked_metrics_batchwise(outputs, labels, mask):
 
     # Calculate metrics for each batch
     iou = true_positives / (
-                true_positives + false_positives + false_negatives + 1e-6)  # Added epsilon for numerical stability
+            true_positives + false_positives + false_negatives + 1e-6)  # Added epsilon for numerical stability
     precision = true_positives / (true_positives + false_positives + 1e-6)
     recall = true_positives / (true_positives + false_negatives + 1e-6)
     f1 = 2 * (precision * recall) / (precision + recall + 1e-6)  # Added epsilon for F1 calculation
@@ -51,6 +51,8 @@ def dice_loss_with_mask_batch_new(outputs, labels, mask):
     # Compute dice loss per batch and average
     dice_loss = 1 - (2. * intersection) / union
     return dice_loss.mean()
+
+
 def dice_loss_with_mask_batch(outputs, labels, mask):
     # all 3 input variables should be shape (batch_size, label_size, label_size)
     # outputs should be sigmoided (0-1)
@@ -86,23 +88,23 @@ def binary_cross_entropy_with_mask_batch(outputs, labels, mask):
 
 
 def create_data(cfg):
-    label = np.ones((cfg.patch_size, cfg.patch_size))
+    label = np.zeros((cfg.patch_size, cfg.patch_size))
     start = (cfg.patch_size // 2) - 32
     end = (cfg.patch_size // 2) + 32
-    label[start + 70:end + 70, start:end] = 0
-    label[0:64, -64:-1] = 0
+    label[start + 70:end + 70, start:end] = 1
+    label[start + 20:start + 40, :] = 1
+    label[0:64, -64:-1] = 1
 
     # get random numpy array of label shape
     rarr = np.random.rand(*label.shape)
 
-
     # image = np.stack([label] * 16)
-    image = np.stack([np.ones_like(label)] * 16)
+    image = np.stack([np.zeros_like(label)] * 16)
     image = np.stack([rarr] * 16)
     # make image random vectors of the same shape
 
-
-    label = resize(label, ((cfg.patch_size // 4), (cfg.patch_size // 4)), order=0, preserve_range=True, anti_aliasing=False)
+    label = resize(label, ((cfg.patch_size // 4), (cfg.patch_size // 4)), order=0, preserve_range=True,
+                   anti_aliasing=False)
     plt.imshow(label, cmap='gray', vmin=0, vmax=1)
     plt.title("label")
     plt.show()
@@ -132,7 +134,6 @@ if __name__ == "__main__":
     plt.show()
 
     mask = torch.tensor(mask_np, dtype=float16).to('cuda')
-
     label = torch.tensor(label, dtype=float16).to('cuda')
 
     # plt.imshow(label, cmap='gray')
@@ -141,30 +142,36 @@ if __name__ == "__main__":
 
     image = torch.Tensor(image).unsqueeze(0).unsqueeze(0).float()
     assert image.dim() == 5
-    print("Image Shape:", image.shape)
+    # print("Image Shape:", image.shape)
 
     image = image.to('cuda')
 
     # 256, dice, 1e-4
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=2e-4, weight_decay=0.01)
     epochs = 200
-    criterion = torch.nn.BCEWithLogitsLoss()
-    for x in tqdm(range(epochs)):
+
+    # criterion = torch.nn.BCEWithLogitsLoss()
+    focal_loss_fn = FocalLoss2d(gamma=0.5)
+
+    for x in range(epochs):
         # Forward Pass
-        logits = model(image)
+        logits = model(image)  # N, H, W
         logits = logits.half()
+
+        focal_loss = focal_loss_fn(logits, label)
 
         probabilities = torch.sigmoid(logits)
 
         # bce_loss = binary_cross_entropy_with_mask_batch(probabilities, label.unsqueeze(0), mask.unsqueeze(0))
         dice_loss = dice_loss_with_mask_batch_new(probabilities, label.unsqueeze(0), mask.unsqueeze(0))
-        iou, precision, recall, f1 = calculate_masked_metrics_batchwise(probabilities, label.unsqueeze(0), mask.unsqueeze(0))
-        # print all metrics in one line
-        print("IoU:", iou.item(), "Precision:", precision.item(), "Recall:", recall.item(), "F1:", f1.item())
 
-        # total_loss = bce_loss + dice_loss
+        # iou, precision, recall, f1 = calculate_masked_metrics_batchwise(probabilities, label.unsqueeze(0),
+        #                                                                 mask.unsqueeze(0))
+        # print all metrics in one line
+        # print("IoU:", iou.item(), "Precision:", precision.item(), "Recall:", recall.item(), "F1:", f1.item())
+
+        total_loss = dice_loss + focal_loss
         # total_loss = dice_loss
-        total_loss = criterion(logits, label.unsqueeze(0))
 
         optimizer.zero_grad()
         total_loss.backward()
