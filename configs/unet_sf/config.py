ec6b1dd71ffbc8cbdeefd20bcb3514d2f2601317
Marcel Roth <MarcelRoth100@aol.com>
2023-12-17 02:15:57 +0100

    add gpu setup shell script

--- configs/unet_sf/config.py
+++ configs/unet_sf/config.py
@@ -48,8 +48,8 @@ step_lr_steps = 1
 step_lr_factor = 0.98
 weight_decay = 0.001
 
-num_workers = 32
-train_batch_size = 4
+num_workers = 16
+train_batch_size = 2
 val_batch_size = train_batch_size
 
 # TRAIN AUG AND VAL AUG HAVE TO BE LAST PARAMETERS OF CONFIG IN THIS ORDER
