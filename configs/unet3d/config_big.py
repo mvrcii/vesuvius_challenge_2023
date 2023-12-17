7dec4008285563b0994909767541538cd9e7dba1
Marcel Roth <MarcelRoth100@aol.com>
2023-12-17 15:41:34 +0100

    cleanup of models

--- configs/unet3d/config_big.py
+++ configs/unet3d/config_big.py
@@ -37,16 +37,15 @@ val_interval = 1
 patch_size = 512
 label_size = patch_size // 4
 stride = patch_size // 2
-ink_ratio = 3
 fragment_ids = [BLASTER_FRAG_ID, IRONHIDE_FRAG_ID, THUNDERCRACKER_FRAG_ID, JETFIRE_FRAG_ID, GRIMLARGE_FRAG_ID,
                 JAZZILLA_FRAG_ID]
 validation_fragments = [HOT_ROD_FRAG_ID]
 train_split = 0.8
 
-take_full_dataset = False
+take_full_dataset = True  # If set to true, takes full dataset and ignores thresholds below
+ink_ratio = 3
 no_ink_sample_percentage = 0.2  # Given the count of N ink samples, take this percentage of the count as no ink
 max_ignore_th = 10  # For no ink samples, how much ignore percentage (%) is allowed maximum
-
 
 lr = 5e-5
 step_lr_steps = 1
