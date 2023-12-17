ade8ec38f29f093b67930f79f7bb584a29082c9f
Marcel Roth <MarcelRoth100@aol.com>
2023-12-17 03:19:17 +0100

    cleanup of models

--- train.py
+++ train.py
@@ -143,6 +143,7 @@ def main():
         accelerator="auto",
         devices=devices,
         enable_progress_bar=True,
+        find_unused_parameters=True,
         precision='16-mixed',
         gradient_clip_val=1.0,
         gradient_clip_algorithm="norm",
