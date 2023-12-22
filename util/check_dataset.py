2a48582cc35fd819eaa6ac1a7298aceaa40e65fb
Marcel Roth <MarcelRoth100@aol.com>
2023-12-22 23:30:39 +0100

    update

--- util/check_dataset.py
+++ util/check_dataset.py
@@ -2,9 +2,9 @@ import os
 
 import numpy as np
 
-for frag_dir in os.listdir(os.path.join('data', 'datasets')):
+for frag_dir in os.listdir(os.path.join('data', 'datasets', '512')):
     nan_count = 0
-    for file in os.listdir(os.path.join('data', 'datasets', frag_dir, 'images')):
+    for file in os.listdir(os.path.join('data', 'datasets', '512', frag_dir, 'images')):
         arr = np.load(file)
         if np.isnan(arr).any():
             nan_count += 1
