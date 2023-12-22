294f049bcab0253d01ea81ae3b594f62cc0a3df5
Marcel Roth <MarcelRoth100@aol.com>
2023-12-22 23:25:59 +0100

    update

--- util/get_inference_for_checkpoint.py
+++ util/get_inference_for_checkpoint.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from constants import JAZZBIGGER_FRAG_ID, GRIMHUGE_FRAG_ID, BLUEBIGGER_FRAG_ID, SKYBIGGER_FRAG_ID, DEVASBIGGER_FRAG_ID, \
-    TRAILBREAKER_FRAG_ID
+    TRAILBREAKER_FRAG_ID, TITLE1_FRAG_ID, TITLE2_FRAG_ID
 from get_inference import get_inference_folder
 
 sys.path.append('../')
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     meta = AlphaBetaMeta()
 
     fragments = [BLUEBIGGER_FRAG_ID, GRIMHUGE_FRAG_ID, SKYBIGGER_FRAG_ID, DEVASBIGGER_FRAG_ID, TRAILBREAKER_FRAG_ID,
-                 JAZZBIGGER_FRAG_ID]
+                 JAZZBIGGER_FRAG_ID, TITLE1_FRAG_ID, TITLE2_FRAG_ID]
     checkpoint = meta.get_previous_model()
 
     get_inference_for_checkpoint(frag_ids=fragments, checkpoint_path=checkpoint, host="vast")
