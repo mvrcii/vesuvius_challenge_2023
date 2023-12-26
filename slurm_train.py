d41a9676ceb7a501b877a600f4d51cfb56ac159a
Marcel Roth <MarcelRoth100@aol.com>
2023-12-26 14:09:58 +0100

    update

---
+++ slurm_train.py
@@ -0,0 +1,23 @@
+import argparse
+import subprocess
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Submit a training job to Slurm.")
+    parser.add_argument("config_path", type=str, help="Path to the config file.")
+    parser.add_argument('--seed', type=int, default=None, help='Optional seed for the script')
+
+    args = parser.parse_args()
+
+    seed_str = f"--seed {args.seed}" if args.seed else ""
+    cmd_str = f"python3 train.py {args.config_path} {seed_str}"
+
+    slurm_cmd = f'sbatch \
+    --wrap="{cmd_str}" \
+    -o "slurm_logs/slurm-%j.out"'
+
+    subprocess.run(slurm_cmd, shell=True)
+
+
+if __name__ == "__main__":
+    main()
