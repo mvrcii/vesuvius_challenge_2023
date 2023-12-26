95355d1f12f03d6e524efae3478e6828b28295a7
Marcel Roth <MarcelRoth100@aol.com>
2023-12-26 14:18:30 +0100

    update

--- slurm_train.py
+++ slurm_train.py
@@ -12,9 +12,7 @@ def main():
     seed_str = f"--seed {args.seed}" if args.seed else ""
     cmd_str = f"python3 train.py {args.config_path} {seed_str}"
 
-    slurm_cmd = f'sbatch \
-    --wrap="{cmd_str}" \
-    -o "logs/slurm-%j.out"'
+    slurm_cmd = f'sbatch --wrap="{cmd_str}" -o "logs/slurm-%j.out"'
 
     subprocess.run(slurm_cmd, shell=True)
 
