46220479b37851e879531b77d56cad05597d6ae2
JakeGonder <gonderjake@gmail.com>
2023-12-26 02:56:52 +0100

    cleanup train

--- configs/segmentation/unet3d_sf/config.py
+++ configs/segmentation/unet3d_sf/config.py
@@ -9,36 +9,34 @@ _base_ = [
     "configs/schedules/adamw_cosine_lr.py",
 ]
 
+# MODEL TYPE
 segmentation = True
-work_dir = os.path.join("/scratch", "medfm", "vesuv", "kaggle1stReimp")
-base_label_dir = os.path.join("data", "base_label_files")
-data_root_dir = "data"
-dataset_target_dir = os.path.join("multilayer_approach", "datasets")
 
-# training parameters
-model_type = "b2"
-segformer_from_pretrained = f"nvidia/mit-{model_type}"
-# from_pretrained = "playful-glade-812-unetr-sf-b3-231216-041654"
-architecture = 'unet3d-sf'
-model_name = f"{architecture}-{model_type}"
-
+# DATASET CREATION
 in_chans = 12
-seed = 97074
-epochs = -1
-dataset_fraction = 1
-unet3d_out_channels = 16
-
-val_interval = 1
-
-# dataset creation parameters
 patch_size = 512
 label_size = patch_size // 4
 stride = patch_size // 2
 ink_ratio = 15
 fragment_ids = [BLASTER_FRAG_ID, IRONHIDE_FRAG_ID, THUNDERCRACKER_FRAG_ID, JETFIRE_FRAG_ID, GRIMLARGE_FRAG_ID,
                 JAZZILLA_FRAG_ID]
+
+
+# training parameters
 validation_fragments = [HOT_ROD_FRAG_ID]
-train_split = 0.8
+model_type = "b2"
+segformer_from_pretrained = f"nvidia/mit-{model_type}"
+# from_pretrained = "playful-glade-812-unetr-sf-b3-231216-041654"
+architecture = 'unet3d-sf'
+model_name = f"{architecture}-{model_type}"
+
+no_ink_sample_percentage = 1
+dataset_fraction = 1
+seed = 97074
+epochs = -1
+unet3d_out_channels = 16
+
+val_interval = 1
 
 lr = 1e-4  # 1e-4
 step_lr_steps = 1
@@ -52,6 +50,12 @@ focal_alpha = 0.75
 num_workers = 1
 train_batch_size = 2
 val_batch_size = train_batch_size
+
+# PATHS
+work_dir = os.path.join("/scratch", "medfm", "vesuv", "kaggle1stReimp")
+base_label_dir = os.path.join("data", "base_label_files")
+data_root_dir = "data"
+dataset_target_dir = os.path.join("multilayer_approach", "datasets")
 
 # TRAIN AUG AND VAL AUG HAVE TO BE LAST PARAMETERS OF CONFIG IN THIS ORDER
 train_aug = [
