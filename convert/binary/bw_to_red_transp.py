b3b02b3f9241db835cdeac05677be66f5c405701
JakeGonder <gonderjake@gmail.com>
2023-12-27 01:04:51 +0100

    a

---
+++ convert/binary/bw_to_red_transp.py
@@ -0,0 +1,27 @@
+from PIL import Image
+from tqdm import tqdm
+
+Image.MAX_IMAGE_PIXELS = None
+
+
+path = "20231012184422_inklabels_32_35.png"
+processed = Image.open(path)
+
+processed = processed.convert('RGBA')
+datas = processed.getdata()
+
+newData = []
+
+print("start converting")
+for item in tqdm(datas):
+    # Check if the pixel is white
+    if item[:3] == (255, 255, 255):
+        # Change white to red, keep the original alpha
+        newData.append((255, 0, 0, item[3]))
+    else:
+        # Make non-white pixels transparent
+        newData.append((0, 0, 0, 0))
+processed.putdata(newData)
+
+processed.save("transparent" + path)
+print("done saving")
