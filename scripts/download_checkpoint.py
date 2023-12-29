8920d988a07c55a81fad6a296ac12f8d31c0445c
Marcel Roth <MarcelRoth100@aol.com>
2023-12-29 17:25:04 +0100

    add slurm batch inference

--- scripts/download_checkpoint.py
+++ scripts/download_checkpoint.py
@@ -7,11 +7,7 @@ GREEN = "\033[92m"
 RESET = "\033[0m"
 
 
-def download_checkpoint(checkpoint_name):
-    # Assuming there's only one hostname since the original script had a single option
-    hostname = "vast"
-
-    # Check if checkpoint already exists
+def download_checkpoint(checkpoint_name, hostname):
     checkpoint_dir = os.path.join("checkpoints", checkpoint_name)
     if os.path.isdir(checkpoint_dir):
         print(f"{GREEN}Checkpoint folder already exists. Exiting to prevent overwrite.{RESET}")
@@ -20,13 +16,14 @@ def download_checkpoint(checkpoint_name):
     print(f"{GREEN}Download process initiated!{RESET}")
 
     full_server_path = "~/kaggle1stReimp/checkpoints"
-    command = f'scp -r "{hostname}:{full_server_path}/{checkpoint_name}" "checkpoints/{checkpoint_name}"'
+    command = f'scp -r "{hostname}:{full_server_path}/{checkpoint_name}" "checkpoints"'
     subprocess.run(command)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Download a checkpoint.")
     parser.add_argument('checkpoint_name', help="Name of the checkpoint to download")
+    parser.add_argument('hostname', help="The hostname to download froam")
     args = parser.parse_args()
 
-    download_checkpoint(args.checkpoint_name)
+    download_checkpoint(args.checkpoint_name, args.hostname)
