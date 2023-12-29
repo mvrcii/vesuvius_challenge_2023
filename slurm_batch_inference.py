134b89275cbd1f905f7202585aa6eadcb49479f9
Marcel Roth <MarcelRoth100@aol.com>
2023-12-29 22:05:28 +0100

    add jazzbigger labels it2

--- slurm_batch_inference.py
+++ slurm_batch_inference.py
@@ -30,14 +30,14 @@ def process_output(frag_id, command, tta, stride, checkpoint):
 def main():
     available_nodes = [2]
     excluded_gpus_node_one = {1, 3, 5}  # Exclude reserved-164-01 gpus here
-    excluded_gpus_node_two = {0, 1, 3, 4, 5, 6, 7}  # Exclude reserved-237-02 gpus here
+    excluded_gpus_node_two = {0, 1, 3, 5, 6}  # Exclude reserved-237-02 gpus here
 
     available_gpus = [gpu_id for gpu_id in range(0, 8)]
     available_gpu_combinations = [(node_id, gpu_id) for node_id in available_nodes for gpu_id in available_gpus
                                   if gpu_id not in excluded_gpus_node_one and node_id == 1
                                   or gpu_id not in excluded_gpus_node_two and node_id == 2]
 
-    tta = True
+    tta = False
     stride = 2
     checkpoint = "efficient-aardvark-1173-unetr-sf-b5-231229-082126"
 
@@ -48,7 +48,7 @@ def main():
     # ]
 
     frags_2_infer = [
-        IRONHIDE_FRAG_ID, ULTRA_MAGNUS_FRAG_ID
+        GRIMHUGE_FRAG_ID, THUNDERCRACKER_FRAG_ID
     ]
 
     for frag_id, (node_id, gpu_id) in zip(frags_2_infer, available_gpu_combinations):
