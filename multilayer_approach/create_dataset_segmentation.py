51306981b594043a8be0d832423c2c383361add0
Marcel Roth <MarcelRoth100@aol.com>
2023-12-30 07:13:52 +0100

    update unetr b5 config

--- multilayer_approach/create_dataset_segmentation.py
+++ multilayer_approach/create_dataset_segmentation.py
@@ -299,7 +299,7 @@ if __name__ == '__main__':
     LABEL_INFO_LIST = []
 
     # todo fix path
-    label_dir = os.path.join(cfg.work_dir, "data", "labels", "twelve_layer_unetr", "binarized")
+    label_dir = os.path.join(cfg.work_dir, "data", "labels", "twelve_layer_unetr_it2", "binarized")
     print("Using label dir:", label_dir)
 
     train_fragments = cfg.fragment_ids
