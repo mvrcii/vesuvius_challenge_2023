c08f6d2e86edd2911ddb96593bb017000c878661
JakeGonder <gonderjake@gmail.com>
2023-12-30 20:31:17 +0100

    x

--- scripts/tools/send/scale_down.py
+++ scripts/tools/send/scale_down.py
@@ -9,11 +9,11 @@ if not os.path.exists('scaled_down'):
     os.makedirs('scaled_down')
 
 # Iterate through all files in the current directory
-for filename in tqdm(os.listdir('')):
+for filename in tqdm(os.listdir('.')):
     if filename.endswith('.png'):
         img = Image.open(filename)
         # Scale the image down to 25% of its size
-        factor = 2
+        factor = 4
         img = img.resize((img.width // factor, img.height // factor))
         # Save the scaled image in the subfolder
         img.save(f'scaled_down/{filename}')
