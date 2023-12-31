dd03a028ecb0fb475042357726031d9fbb17c58b
Marcel Roth <MarcelRoth100@aol.com>
2023-12-31 18:57:34 +0100

    add important configs

--- slurm_inference.py
+++ slurm_inference.py
@@ -3,7 +3,9 @@ import re
 import subprocess
 import time
 
-from utility.fragments import get_frag_name_from_id
+from scripts.download_inference_multilayer import dynamic_closest_matches, get_checkpoint_name
+from utility.checkpoints import CHECKPOINTS
+from utility.fragments import get_frag_name_from_id, FragmentHandler
 
 
 def print_colored(message, color, _print=True):
@@ -20,10 +22,49 @@ def print_colored(message, color, _print=True):
         return f"{colors[color]}{message}{colors['end']}"
 
 
+def get_fragment_id(fragment_id_or_name):
+    fragment_ids = FragmentHandler().get_ids()
+    fragment_names = FragmentHandler().get_names()
+    name_to_id = FragmentHandler().FRAGMENTS
+
+    # Check if input is an ID or a Name and convert to ID if necessary
+    if fragment_id_or_name in fragment_ids or fragment_id_or_name in name_to_id:
+        fragment_id = name_to_id.get(fragment_id_or_name, fragment_id_or_name)
+    else:
+        id_suggestions = dynamic_closest_matches(fragment_id_or_name, fragment_ids)
+        name_suggestions = dynamic_closest_matches(fragment_id_or_name, fragment_names)
+        suggestions = list(set(id_suggestions + name_suggestions))
+
+        if suggestions:
+            if len(suggestions) > 1:
+                print("Did you mean one of the following?")
+                for idx, suggestion in enumerate(suggestions, 1):
+                    print(f"{idx}. {suggestion}")
+
+                choice = input("Enter the number of the correct option, or 'n' to cancel: ")
+                if choice.isdigit() and 1 <= int(choice) <= len(suggestions):
+                    selected_suggestion = suggestions[int(choice) - 1]
+                    print(selected_suggestion)
+                    fragment_id = name_to_id.get(selected_suggestion, selected_suggestion)
+                else:
+                    print("Invalid selection.")
+                    exit()
+            elif len(suggestions) == 1:
+                fragment_id = name_to_id.get(suggestions[0])
+            else:
+                print("No valid suggestion found.")
+                exit()
+        else:
+            print_colored(f"No close match found for: {fragment_id_or_name}", "red")
+            exit()
+
+    return fragment_id
+
+
 def main():
     parser = argparse.ArgumentParser(description="Submit a inference job to Slurm.")
     parser.add_argument("checkpoint_path", type=str, help="Path to the checkpoint file.")
-    parser.add_argument('fragment_id', type=str, help='The fragment to infer.')
+    parser.add_argument('fragment_id_or_name', type=str, help='The fragment to infer.')
     parser.add_argument('--stride', type=int, default=2, help='Stride (default: 2)')
     parser.add_argument('--gpu', type=int, default=0, help='GPU (default: 0)')
     parser.add_argument('--tta', action='store_true', help='Perform advanced TTA')
@@ -32,18 +73,24 @@ def main():
     parser.add_argument('--full_sweep', action='store_true', help='Do a full layer inference sweep (0-63)')
     args = parser.parse_args()
 
+    fragment_id = get_fragment_id(fragment_id_or_name=args.fragment_id)
+    checkpoint_path = get_checkpoint_name(args.checkpoint_path, checkpoint_dict=CHECKPOINTS)
+
     tta_str = "_tta" if args.tta else ""
 
     node_name = "tenant-ac-nowak-h100-reserved-237-02" if args.node2 else "tenant-ac-nowak-h100-reserved-164-01"
     script_name = f"multilayer_approach/infer_layered_segmentation_padto16{tta_str}.py"
     print_colored(f"INFO:\tUsing {script_name}", "blue")
 
-    command = ["python3", str(script_name), str(args.checkpoint_path), str(args.fragment_id),
+    command = ["python3", str(script_name), str(checkpoint_path), str(fragment_id),
                '--stride', str(args.stride),
                '--gpu', str(args.gpu)]
 
     if not args.tta and args.full_sweep:
         command.append('--full_sweep')
+
+    print(" ".join(command))
+    exit()
 
     slurm_cmd = f'sbatch --nodelist={node_name} --wrap="{" ".join(command)}" -o "logs/slurm-%j.out"'
 
