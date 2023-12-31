0e8a988b2bd5451a539a999490fb0fbdc68dcd5a
JakeGonder <gonderjake@gmail.com>
2023-12-31 21:07:49 +0100

    update requirements

--- check_inferences.py
+++ check_inferences.py
@@ -1,22 +1,28 @@
 import os
-
 import numpy as np
+import argparse
 
-report_zero_percent = 0.03  # if file has > 60% zeros, print something
-runs_to_ckeck = ["olive-wind-1194"]
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Check for files with > x% zeros.')
+parser.add_argument('report_zero_percent', type=float, help='Percent threshold for zeros')
+parser.add_argument('runs_to_check', type=str, help='Comma-separated list of run names')
+args = parser.parse_args()
+
+report_zero_percent = args.report_zero_percent
+runs_to_check = args.runs_to_check.split(',')
 
 print("Checking:")
-for x in runs_to_ckeck:
+for x in runs_to_check:
     print(x)
 
 # Path to the fragments directory
 fragments_dir = os.path.expanduser("~/kaggle1stReimp/inference/results")  # Update with the path to your fragments directory
-print("Searching in ", fragments_dir)
+print("Searching in", fragments_dir)
 
 
-# Function to check if an array has more than 60% zeros
-def has_more_than_60_percent_zeros(array):
-    return np.count_nonzero(array == 0) / array.size > report_zero_percent
+# Function to check if an array has more than x% zeros
+def has_more_than_x_percent_zeros(array, x):
+    return np.count_nonzero(array == 0) / array.size > x
 
 
 # Iterate over each fragment directory
@@ -27,8 +33,8 @@ for fragment_id in os.listdir(fragments_dir):
         # Check each run name directory
         for sub_dir in os.listdir(fragment_path):
             # print("Checking", sub_dir)
-            for run_name in runs_to_ckeck:
-                if not run_name in sub_dir:
+            for run_name in runs_to_check:
+                if run_name not in sub_dir:
                     # print(sub_dir)
                     # print("does not end with ", run_name)
                     continue
@@ -40,7 +46,7 @@ for fragment_id in os.listdir(fragments_dir):
                         if file.endswith(".npy"):
                             file_path = os.path.join(run_path, file)
                             array = np.load(file_path)
-                            if has_more_than_60_percent_zeros(array):
-                                print(f"File with >{report_zero_percent} zeros: {file_path}")
+                            if has_more_than_x_percent_zeros(array, report_zero_percent):
+                                print(f"File with >{report_zero_percent*100}% zeros: {file_path}")
                             else:
                                 print(f"{file} is correct")
