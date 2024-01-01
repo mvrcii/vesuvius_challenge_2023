1024649e03ca0b8e115e7243279b4884834fea60
Marcel Roth <MarcelRoth100@aol.com>
2024-01-01 01:12:52 +0100

    add important configs

--- check_inferences.py
+++ check_inferences.py
@@ -121,7 +121,7 @@ def check_fragment_dir(checkpoints_to_check, inference_root_dir, threshold, work
             checkpoint = checkpoint.split('_')[-1]
 
             if checkpoint not in checkpoints_to_check:
-                print_colored(f"SKIP: Checkpoint not in checkpoints to check: {checkpoint}", 'blue')
+                print_colored(f"SKIP:\tCheckpoint not in checkpoints to check: {checkpoint}", 'blue')
                 continue
 
             checkpoint_dir = os.path.join(fragment_path, checkpoint)
