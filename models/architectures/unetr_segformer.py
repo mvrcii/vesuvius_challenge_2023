651158df39ba74c450db1bfe38c9f19822a653c5
Marcel Roth <MarcelRoth100@aol.com>
2024-02-21 14:55:53 +0100

    cleaning

--- models/architectures/unetr_segformer.py
+++ models/architectures/unetr_segformer.py
@@ -14,40 +14,6 @@ class CFG:
     patch_size = 128
     unetr_out_channels = 32
 
-    # ============== augmentation =============
-    # train_aug_list = [
-    #     # A.RandomResizedCrop(
-    #     #     size, size, scale=(0.85, 1.0)),
-    #     A.Resize(size, size),
-    #     A.HorizontalFlip(p=0.5),
-    #     A.VerticalFlip(p=0.5),
-    #     A.RandomBrightnessContrast(p=0.75),
-    #     A.ShiftScaleRotate(p=0.75),
-    #     A.OneOf([
-    #         A.GaussNoise(var_limit=[10, 50]),
-    #         A.GaussianBlur(),
-    #         A.MotionBlur(),
-    #     ], p=0.4),
-    #     A.GridDistortion(num_steps=5, distort_limit=0.3, p=0.5),
-    #     A.CoarseDropout(max_holes=1, max_width=int(size * 0.3), max_height=int(size * 0.3),
-    #                     mask_fill_value=0, p=0.5),
-    #     # A.Cutout(max_h_size=int(size * 0.6),
-    #     #          max_w_size=int(size * 0.6), num_holes=1, p=1.0),
-    #     A.Normalize(
-    #         mean=[0] * in_chans,
-    #         std=[1] * in_chans
-    #     ),
-    #     ToTensorV2(transpose_mask=True),
-    # ]
-    #
-    # valid_aug_list = [
-    #     A.Resize(size, size),
-    #     A.Normalize(
-    #         mean=[0] * in_chans,
-    #         std=[1] * in_chans
-    #     ),
-    #     ToTensorV2(transpose_mask=True),
-    # ]
 
 
 class UNETR_Segformer(nn.Module):
